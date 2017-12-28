9f825e6db687094ba585aa49a881b7219475469b
ramesh-daffodil <ramesh.bansal@daffodilsw.com>
2017-12-28 15:21:05 +0530

    Initial commit for contact us page

--- imports/ui/pages/contactUs.js
+++ imports/ui/pages/contactUs.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
+import Paper from 'material-ui/Paper';
+import { withStyles } from "material-ui/styles";
+import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import Grid from 'material-ui/Grid';
 
 const insertPadding = {
   paddingRight: '8px',
   paddingLeft: '8px'
 };
 
-export default class ContactUs extends React.Component {
+const styles = theme => ({
+  backColor: {
+    height:400
+  },
+  textColor: {
+    color: '#ffffff'
+  },
+  gridProps: {
+    padding: 16,
+    color: theme.palette.text.secondary
+  },
+  card: {
+    display: 'inline-block',
+    position: 'relative',
+    width: '100%',
+    margin: '25px 0',
+    'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.14)',
+    'border-radius': 6,
+    background: '#fff',
+    height: 400
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+    'background-color': '#68B3C8',
+    color: '#ffffff'
+  }),
+});
+
+class ContactUs extends React.Component {
 
   constructor(props){
     super(props);
   }
-  
+
   componentDidMount() {
     $(".social_icon").hover(function (e) {
       $(this).addClass('animated tada');
@@ -20,15 +60,15 @@ export default class ContactUs extends React.Component {
     $(".social_icon").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
       $(this).removeClass('animated tada');
     });
-  }  
-    
+  }
+
   submit = (event) => {
         event.preventDefault();
         const name = this.refs.name.value;
         const email = this.refs.email.value;
         const message = this.refs.yourMessage.value;
-        const selectedOption = $("input[name=optionsRadios]:checked").val();   
-         
+        const selectedOption = $("input[name=optionsRadios]:checked").val();
+
         const emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
         if(!email) {
             toastr.error('Please enter your email.','Error');
@@ -54,21 +94,25 @@ export default class ContactUs extends React.Component {
     };
 
     render() {
+        const { classes } = this.props;
+        console.log("Props====>",this.props);
         return(
             <div>
-                <div className="content" >
-                    <div className="container-fluid">
+                 <div className="content">
+                    <div className={classes.card}>
                         <div className="row">
-                            <div className="row card">
-                                <div className="alert alert-info">
-                                    <span style={{fontSize:'24px'}}>Send Us Feedback</span>
-                                </div>
-                                <div className="col-md-7">
-                                    <div className="row">
+                            <Grid container spacing={24}>
+                                <Grid item xs={12}>
+                                    <Paper className={classes.root} elevation={4}>
+                                        <Typography className={classes.textColor} type="headline" component="h3">Send Us Feedback</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <div className= {classes.gridProps}>
                                         <form method="post" onSubmit={this.submit} name="contact" noValidate="novalidate">
                                             <div className="col-md-6">
                                                 <div className="form-group">
-                                                    <input
+                                                    <TextField
                                                         type="text"
                                                         id="name"
                                                         className="form-control"
@@ -78,7 +122,7 @@ export default class ContactUs extends React.Component {
                                                     />
                                                 </div>
                                                 <div className="form-group">
-                                                    <input
+                                                    <TextField
                                                         type="email"
                                                         id="email"
                                                         className="form-control"
@@ -130,7 +174,7 @@ export default class ContactUs extends React.Component {
                                             </div>
                                             <div className="col-md-12 resbtn">
                                                 <div className="form-group">
-                                                    <textarea
+                                                    <TextField
                                                         className="form-control"
                                                         id="message"
                                                         name="message"
@@ -148,8 +192,8 @@ export default class ContactUs extends React.Component {
                                             </div>
                                         </form>
                                     </div>
-                                </div>
-                                <div className="col-md-5 text-center">
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
                                     <ul className="address-info">
                                         <img src="/images/new-logo.png" className="" alt="logo" width="48" style={{width: 'auto', height: '250px'}} />
                                         <div  className="sl-bar">
@@ -160,8 +204,8 @@ export default class ContactUs extends React.Component {
                                             <a style={insertPadding} href="#" className="fa fa-instagram social_icon "></a>
                                         </div>
                                     </ul>
-                                </div>
-                            </div>
+                                </Grid>
+                            </Grid>
                         </div>
                     </div>
                 </div>
@@ -169,3 +213,5 @@ export default class ContactUs extends React.Component {
         )
     }
 }
+
+export default withStyles(styles)(ContactUs);
