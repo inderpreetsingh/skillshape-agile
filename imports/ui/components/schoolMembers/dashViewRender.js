154f3e0506918031f2745a1c59dc14d9e4b6e588
ramesh-daffodil <ramesh.bansal@daffodilsw.com>
2018-02-19 13:39:54 +0530

    Created seperate component for Member details

--- imports/ui/components/schoolMembers/dashViewRender.js
+++ imports/ui/components/schoolMembers/dashViewRender.js
@@ -20,6 +20,7 @@ import isEmpty from "lodash/isEmpty";
 
 import SchoolMemberListItems from '/imports/ui/components/schoolMembers/schoolMemberList/index.js';
 import  SchoolMemberFilter  from "./filter";
+import SchoolMemberDetails from "./schoolMemberDetails"
 import MemberDialogBox from "/imports/ui/components/landing/components/dialogs/MemberDetails.jsx";
 import { ContainerLoader } from '/imports/ui/loading/container.js';
 
@@ -79,49 +80,7 @@ export default function DashViewRender() {
               </div>
         </Grid>
         <Grid item sm={8} xs={12} md={8} className="rightPanel">
-          { !isEmpty(memberInfo) &&
-            <Fragment>
-              <Grid container className="userInfoPanel" style={{display: 'flex',background: '#9cd1ff'}}>
-                <Grid item sm={4} xs={12} md={4}>
-                  <div className="avtar">
-                    <img src="/images/avatar.jpg"/>
-                  </div>
-                  <Typography>{ memberInfo.name }</Typography>
-                  <Typography>{ memberInfo.phone }</Typography>
-                  <Typography>{ memberInfo.email }</Typography>
-                </Grid>
-                <Grid item sm={4} xs={12} md={4} >
-                  <div className="notes">
-                    Admin Notes:
-                  </div>
-                  <Input
-                    onBlur={this.saveAdminNotesInMembers}
-                    value={memberInfo.adminNotes || ""}
-                    onChange={this.handleInput}
-                    fullWidth
-                    style={{border: '1px solid',backgroundColor: '#fff'}}
-                    multiline
-                    rows={4}
-                  />
-                </Grid>
-              </Grid>
-              <Grid container style={{backgroundColor: 'rebeccapurple'}}>
-                <Grid item>
-                  <Fragment>
-                    <Button raised color="primary" style={{margin: '5px'}}>
-                      Call
-                    </Button>
-                    <Button style={{margin: '5px'}} raised color="accent">
-                      Email
-                    </Button>
-                    <Button raised color="primary" style={{margin: '5px'}}>
-                      Edit
-                    </Button>
-                  </Fragment>
-                </Grid>
-              </Grid>
-            </Fragment>
-          }
+          { !isEmpty(memberInfo) && <SchoolMemberDetails memberInfo={memberInfo}/> }
         </Grid>
       </Grid>
     )
