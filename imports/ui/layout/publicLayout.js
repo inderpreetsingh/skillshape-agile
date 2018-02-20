1cb8d3409c643bb3b9cbf3d077050b49f8fecc68
Ishwerdas <singhs.ishwer@gmail.com>
2018-02-21 02:07:18 +0530

    resolved minor issue

--- imports/ui/layout/publicLayout.js
+++ imports/ui/layout/publicLayout.js
@@ -19,7 +19,7 @@ const styles = theme => ({
     content: {
         backgroundColor: theme.palette.background.default,
         paddingTop: theme.spacing.unit*10 - theme.spacing.unit/2,
-        overflow: hidden,
+        overflow: 'hidden',
     }
 });
 
