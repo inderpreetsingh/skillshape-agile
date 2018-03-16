456dd522a252b1487603f56d059d6f255522488f
Ishwerdas <singhs.ishwer@gmail.com>
2018-03-17 00:50:21 +0530

    see more button and status updated

--- imports/ui/components/landing/components/cards/CardsList.jsx
+++ imports/ui/components/landing/components/cards/CardsList.jsx
@@ -96,19 +96,19 @@ class CardsList extends Component {
     }
 
     seeMoreStatus = (cardsData, filters) => {
-        // const { limit, skillCategoryClassLimit } = filters;
-        // if(limit) {
-        //     if (limit <= size(cardsData)) {
-        //         return true
-        //     }
-        // } else {
-        //     if ((size(cardsData) >= 4 && isEmpty(skillCategoryClassLimit))
-        //         || (skillCategoryClassLimit && skillCategoryClassLimit[name] < size(cardsData))) {
-        //         return true
-        //     }
-        // }
-        // return false;
-        // return true;
+        const { limit, skillCategoryClassLimit } = filters;
+        if(limit) {
+            if (limit <= size(cardsData)) {
+                return true
+            }
+        } else {
+            if ((size(cardsData) >= 4 && isEmpty(skillCategoryClassLimit))
+                || (skillCategoryClassLimit && skillCategoryClassLimit[name] < size(cardsData))) {
+                return true
+            }
+        }
+        return false;
+
     }
 
     render() {
