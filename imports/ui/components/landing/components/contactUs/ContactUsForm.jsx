19e4d186c3b9152d1562f34946c1a846fb733f1f
Ishwerdas <singhs.ishwer@gmail.com>
2018-03-24 00:12:59 +0530

    contact page enhanced

--- imports/ui/components/landing/components/contactUs/ContactUsForm.jsx
+++ imports/ui/components/landing/components/contactUs/ContactUsForm.jsx
@@ -11,16 +11,27 @@ import * as helpers from '../jss/helpers.js';
 
 const styles = {
   formGroupWrapper: {
+    width: '100%',
     margin: `${helpers.rhythmDiv * 2}px 0`,
     marginTop: helpers.rhythmDiv * 4,
-    paddingLeft: helpers.rhythmDiv
+    paddingLeft: helpers.rhythmDiv,
+    [`@media screen and (max-width: ${helpers.mobile + 100}px)`] : {
+      width: 'auto'
+    }
   },
   radioLabelRoot: {
     marginRight: helpers.rhythmDiv * 3
   },
+  radioLabelMarginBottom: {
+    marginBottom: helpers.rhythmDiv * 2,
+    [`@media screen and (max-width: ${helpers.mobile}px)`] : {
+    }
+  },
   radioLabel: {
-    fontFamily: helpers.specialFont,
-    fontSize: helpers.baseFontSize
+    fontSize: helpers.baseFontSize,
+    [`@media screen and (max-width: ${helpers.mobile}px)`] : {
+      fontSize: 14
+    }
   },
   radioButton: {
     height: helpers.rhythmDiv * 3,
@@ -33,9 +44,10 @@ const styles = {
   radioButtonGroup: {
     display: 'flex',
     flexDirection: 'row',
-    flexWrap: 'nowrap',
-    [`@media screen and (max-width: ${helpers.mobile}px)`] : {
-      flexWrap: 'wrap'
+    flexWrap : 'wrap',
+    justifyContent: 'space-between',
+    [`@media screen and (max-width: ${helpers.mobile + 100}px)`] : {
+      justifyContent: 'flex-start'
     }
   },
 }
@@ -45,18 +57,22 @@ const FormWrapper = styled.div`
   padding: ${helpers.rhythmDiv * 2}px;
   padding-bottom: 0;
   max-width: 600px;
-  width: 100%:
+  width: 100%;
   display: flex;
   justify-content: center;
   border: 2px solid #ccc;
 
-  @media screen and (max-width: ${helpers.tablet}px) {
+  @media screen and (max-width: ${helpers.tablet + 50}px) {
     margin-bottom: ${helpers.rhythmDiv * 4}px;
+  }
+
+  @media screen and (max-width: ${helpers.mobile}px) {
+    margin-bottom: ${helpers.rhythmDiv * 2}px;
   }
 `;
 
 const Form = styled.form`
-  width: 80%;
+  width: 86%;
   margin: 0 auto;
 
   @media screen and (max-width : ${helpers.mobile + 100}px) {
@@ -72,6 +88,10 @@ const SubmitButtonWrapper = styled.div`
 
 const ButtonWidthWrapper = styled.div`
   width: 200px;
+`;
+
+const InputWrapper = styled.div`
+  margin-bottom: ${helpers.rhythmDiv}px;
 `;
 
 class ContactUsForm extends Component {
@@ -117,9 +137,13 @@ class ContactUsForm extends Component {
     return (
         <FormWrapper>
           <Form>
-            <IconInput inputId="name" labelText="Your name" value={this.state.name} onChange={this.handleInputFieldChange('name')}/>
+            <InputWrapper>
+              <IconInput inputId="name" labelText="Your name" value={this.state.name} onChange={this.handleInputFieldChange('name')}/>
+            </InputWrapper>
 
-            <IconInput inputId="email" type="email" labelText="Enter your email id" value={this.state.email} onChange={this.handleInputFieldChange('email')} />
+            <InputWrapper>
+              <IconInput inputId="email" type="email" labelText="Enter your email id" value={this.state.email} onChange={this.handleInputFieldChange('email')} />
+            </InputWrapper>
 
             <FormControl component="fieldset" required classes={{root: this.props.classes.formGroupWrapper}}>
                 <RadioGroup
@@ -129,9 +153,9 @@ class ContactUsForm extends Component {
                     className={this.props.classes.radioButtonGroup}
                     onChange={this.handleChange}
                   >
-                    <FormControlLabel classes={{root: this.props.classes.radioLabelRoot , label: this.props.classes.radioLabel}} value="feature" control={<Radio classes={{root : this.props.classes.radioButton}}/>} label="Feature request" />
-                    <FormControlLabel classes={{root: this.props.classes.radioLabelRoot , label: this.props.classes.radioLabel}} value="somethingBroken" control={<Radio classes={{root : this.props.classes.radioButton}}/>} label="Something broken" />
-                    <FormControlLabel classes={{root: this.props.classes.radioLabelRootZeroMargin, label: this.props.classes.radioLabel}} value="other" control={<Radio classes={{root : this.props.classes.radioButton}}/>} label="I love it!" />
+                    <FormControlLabel classes={{root: this.props.classes.radioLabelRoot +' '+ this.props.classes.radioLabelMarginBottom, label: this.props.classes.radioLabel}} value="feature" control={<Radio classes={{root : this.props.classes.radioButton}}/>} label="Feature request" />
+                    <FormControlLabel classes={{root: this.props.classes.radioLabelRoot +' '+ this.props.classes.radioLabelMarginBottom, label: this.props.classes.radioLabel}} value="somethingBroken" control={<Radio classes={{root : this.props.classes.radioButton}}/>} label="Something broken" />
+                    <FormControlLabel classes={{root: this.props.classes.radioLabelRootZeroMargin +' '+ this.props.classes.radioLabelMarginBottom, label: this.props.classes.radioLabel}} value="other" control={<Radio classes={{root : this.props.classes.radioButton}}/>} label="I love it!" />
                 </RadioGroup>
             </FormControl>
 
@@ -140,9 +164,9 @@ class ContactUsForm extends Component {
             <SubmitButtonWrapper>
               <ButtonWidthWrapper>
                 {this.state.readyToSumit ?
-                <PrimaryButton fullWidth type="submit" noMarginBottom label="submit" />
+                <PrimaryButton fullWidth type="submit" noMarginBottom label="Send Message" />
                 :
-                <button className="cancel-button full-width increase-height" disabled >submit</button>}
+                <button className="cancel-button full-width increase-height" disabled >Send Message</button>}
               </ButtonWidthWrapper>
             </SubmitButtonWrapper>
           </Form>
