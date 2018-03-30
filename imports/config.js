d85d1f051fb4b90cdfaf58ae5ff6400c2d929b8f
ramesh-daffodil <ramesh.bansal@daffodilsw.com>
2018-03-30 11:04:35 +0530

    Put captcha key into if check

--- imports/config.js
+++ imports/config.js
@@ -58,7 +58,7 @@ export default config = Object.freeze({
 	// Show only 4 classes initially to Users. This count will increase when user clicks on see more on dash route.
 	seeMoreCount: 4,
 	// CAPTCHA_SITE_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", //test key
-	CAPTCHA_SITE_KEY: "6LeRJE8UAAAAAEtZBraHTNfUe3VD8k5QgJ0oM1J3", // production key
+	CAPTCHA_SITE_KEY: process.env.NODE_ENV == "development" ? "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" : "6LeRJE8UAAAAAEtZBraHTNfUe3VD8k5QgJ0oM1J3", // production key
 	// Need to show these default classes to `Users` if no class type data found for User's current location.
 	defaultClassType: [
 		{ skillType: "Judo", location: "Toyko"},
