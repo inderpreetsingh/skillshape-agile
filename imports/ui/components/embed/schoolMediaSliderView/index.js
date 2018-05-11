cd727a4435e675ad32a5c1254bca2773c45435d9
ramesh-daffodil <ramesh.bansal@daffodilsw.com>
2018-05-11 10:13:13 +0530

    Show media list for ifram code

--- imports/ui/components/embed/schoolMediaSliderView/index.js
+++ imports/ui/components/embed/schoolMediaSliderView/index.js
@@ -8,20 +8,29 @@ import MediaList from "/imports/ui/components/schoolView/editSchool/mediaDetails
 class SchoolMediaSliderView extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      limit: 4
+    };
   }
+
+  changeLimit = () => {
+    let incerementFactor = 1;
+    this.setState({ limit: this.state.limit + incerementFactor });
+  };
 
   render() {
     const { schoolId } = this.props;
     // Get media gallery data of any School on the basis of filters.
     const filters = { schoolId: this.props.schoolId };
-    console.log("this------------",this)
     return (
       <div className="wrapper" style={{ padding: 20 }}>
-        <MediaList
-          changeLimit = {this.changeLimit}
-          limit= {10}
-          schoolId={schoolId}
-        />
+        {this.props.subsReady && (
+          <MediaList
+            changeLimit={this.changeLimit}
+            limit={this.state.limit}
+            schoolId={this.props.schoolId}
+          />
+        )}
       </div>
     );
   }
@@ -29,12 +38,16 @@ class SchoolMediaSliderView extends React.Component {
 
 export default createContainer(props => {
   const { slug } = props.params;
-  Meteor.subscribe("UserSchoolbySlug", slug);
-  const schoolData = School.findOne({ slug: slug });
-  const schoolId = schoolData && schoolData._id;
-  console.log("schoolData=============>", schoolData);
+  let subscription = Meteor.subscribe("UserSchoolbySlug", slug);
+  const subsReady = subscription && subscription.ready();
+  let schoolData, schoolId;
+  if (subsReady) {
+    schoolData = School.findOne({ slug: slug });
+    schoolId = schoolData && schoolData._id;
+  }
   return {
     ...props,
+    subsReady: subsReady,
     schoolId: schoolId
   };
 }, SchoolMediaSliderView);
