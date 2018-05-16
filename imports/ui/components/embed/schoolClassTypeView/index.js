3ae86be2a8d3d1b99bdca1ff38bbe01b9893ce2c
ramesh-daffodil <ramesh.bansal@daffodilsw.com>
2018-05-16 18:48:46 +0530

    Fixed Iframe content height

--- imports/ui/components/embed/schoolClassTypeView/index.js
+++ imports/ui/components/embed/schoolClassTypeView/index.js
@@ -13,6 +13,33 @@ class SchoolClassTypeView extends React.Component {
     }
   }
 
+  componentDidUpdate() {
+      // Get height of document
+      function getDocHeight(doc) {
+          doc = doc || document;
+          // from http://stackoverflow.com/questions/1145850/get-height-of-entire-document-with-javascript
+          var body = doc.body,
+              html = doc.documentElement;
+          var height = Math.max(body.scrollHeight, body.offsetHeight,
+              html.clientHeight, html.scrollHeight, html.offsetHeight);
+          return doc.getElementById('UserMainPanel').offsetHeight;
+      }
+      // send docHeight onload
+      function sendDocHeightMsg(e) {
+        setTimeout(()=> {
+          var ht = getDocHeight();
+          parent.postMessage(JSON.stringify({ 'docHeight': ht, 'iframeId' : 'ss-school-classtype-view'}), '*');
+        }, 3000)
+      }
+      // assign onload handler
+      sendDocHeightMsg();
+      // if (window.addEventListener) {
+      //     window.addEventListener('load', sendDocHeightMsg, false);
+      // } else if (window.attachEvent) { // ie8
+      //     window.attachEvent('onload', sendDocHeightMsg);
+      // }
+  }
+
   handleSeeMore = () => {
     // Attach count with skill cateory name so that see more functionlity can work properly.
     console.log("handleSeeMore");
