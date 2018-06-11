f653d248a3842817545ab498f79d09cfdf015a91
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-06-11 13:05:10 +0530

    class details not showing expended class details fixed

--- imports/ui/components/schoolView/editSchool/classTypeDetails/index.js
+++ imports/ui/components/schoolView/editSchool/classTypeDetails/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
-import ClassTypeDetailsRender from './classTypeDetailsRender';
-import '/imports/api/classPricing/methods';
+import React from "react";
+import { createContainer } from "meteor/react-meteor-data";
+import ClassTypeDetailsRender from "./classTypeDetailsRender";
+import "/imports/api/classPricing/methods";
 
 import ClassType from "/imports/api/classType/fields";
 import SkillCategory from "/imports/api/skillCategory/fields";
@@ -9,99 +9,100 @@ import SkillSubject from "/imports/api/skillSubject/fields";
 import ClassTimes from "/imports/api/classTimes/fields";
 
 class ClassTypeDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null
+    };
+  }
 
-    constructor(props) {
-    	super(props);
-        this.state = {
-           file: null,
+  getChildTableData(parentData) {
+    console.log("getChildTableData -->>", parentData._id);
+    return ClassTimes.find({ classTypeId: parentData._id }).fetch();
+  }
+
+  handleImageChange = file => {
+    this.setState({ file });
+  };
+
+  handleImageSave = (schoolId, classTypeId) => {
+    console.log("<<<< handleImageSave called-->>", this.state);
+    const { file } = this.state;
+    let doc = {
+      schoolId: schoolId
+    };
+    if (file && file.fileData && !file.isUrl) {
+      S3.upload(
+        { files: { "0": file.fileData }, path: "schools" },
+        (err, res) => {
+          if (err) {
+            console.error("err ", err);
+          }
+          if (res) {
+            doc.classTypeImg = res.secure_url;
+            this.editClassType({ doc_id: classTypeId, doc });
+          }
         }
+      );
+    } else if (file && file.isUrl) {
+      doc.classTypeImg = file.file;
+      this.editClassType({ doc_id: classTypeId, doc });
+    } else {
+      doc.classTypeImg = null;
+      this.editClassType({ doc_id: classTypeId, doc });
     }
+  };
 
-    getChildTableData(parentData) {
-        console.log("getChildTableData -->>", parentData._id)
-  		return ClassTimes.find({classTypeId: parentData._id}).fetch();
-    }
-
-  	handleImageChange = (file)=> {
-        this.setState({file})
-  	}
-
-  	handleImageSave = (schoolId, classTypeId) => {
-      console.log("<<<< handleImageSave called-->>",this.state)
-      const { file } = this.state;
-      let doc = {
-        schoolId: schoolId,
+  editClassType = ({ doc, doc_id }) => {
+    Meteor.call("classType.editClassType", { doc, doc_id }, (error, result) => {
+      if (error) {
+        console.error("error", error);
       }
-      if(file && file.fileData && !file.isUrl) {
-        S3.upload({files: { "0": file.fileData}, path:"schools"}, (err, res) => {
-            if(err) {
-                console.error("err ",err);
-            }
-            if(res) {
-                doc.classTypeImg = res.secure_url;
-                this.editClassType({ doc_id: classTypeId, doc})
-            }
-        })
-
-      } else if(file && file.isUrl) {
-        doc.classTypeImg = file.file;
-        this.editClassType({ doc_id: classTypeId, doc})
-      } else {
-        doc.classTypeImg = null;
-        this.editClassType({ doc_id: classTypeId, doc})
+      if (result) {
       }
+    });
+  };
 
-  	}
+  moveToNextTab = () => {
+    this.props.moveToNextTab(3);
+  };
 
-    editClassType = ({ doc, doc_id })=> {
-
-        Meteor.call("classType.editClassType", { doc, doc_id }, (error, result) => {
-            if (error) {
-              console.error("error", error);
-            }
-            if (result) {
-
-            }
-        });
-    }
-
-    moveToNextTab = () => {
-      this.props.moveToNextTab(3);
-    }
-
-    render() {
-    	return ClassTypeDetailsRender.call(this, this.props, this.state)
-    }
+  render() {
+    return ClassTypeDetailsRender.call(this, this.props, this.state);
+  }
 }
 
 export default createContainer(props => {
-    const { schoolId } = props;
-    let classTimesData = [];
-    let classTypeData = [];
+  const { schoolId } = props;
+  let classTimesData = [];
+  let classTypeData = ClassType.find({ schoolId: schoolId }).fetch();
+  let subscription = Meteor.subscribe("classType.getclassType", { schoolId });
 
-    let subscription = Meteor.subscribe("classType.getclassType", {schoolId});
+  if (subscription.ready()) {
+    let classTypeIds = classTypeData && classTypeData.map(data => data._id);
 
-    if(subscription.ready()) {
-        classTypeData = ClassType.find({ schoolId: schoolId }).fetch();
-        let classTypeIds = classTypeData && classTypeData.map((data) => data._id);
+    Meteor.subscribe("classTimes.getclassTimesByClassTypeIds", {
+      schoolId,
+      classTypeIds
+    });
+    classTimesData = ClassTimes.find(
+      { schoolId },
+      { sort: { _id: -1 } }
+    ).fetch();
+  }
 
-        Meteor.subscribe("classTimes.getclassTimesByClassTypeIds", {schoolId, classTypeIds});
-        classTimesData = ClassTimes.find({ schoolId },{sort: {_id: -1}}).fetch();
-    }
-
-    // console.log("classTimesData -->>",classTimesData)
-    /*Find skills to make this container reactive on skill
+  // console.log("classTimesData -->>",classTimesData)
+  /*Find skills to make this container reactive on skill
     other wise skills are joined with collections using package
     perak:joins */
-    SkillCategory.find().fetch()
-    SkillSubject.find().fetch()
-    /*****************************************************/
+  SkillCategory.find().fetch();
+  SkillSubject.find().fetch();
+  /*****************************************************/
 
-    return {
-        ...props,
-        schoolId,
-        classTypeData,
-        classTimesData,
-    };
-
+  return {
+    ...props,
+    schoolId,
+    classTypeData,
+    classTimesData
+  };
 }, ClassTypeDetails);
--- imports/ui/components/schoolView/editSchool/index.js
+++ imports/ui/components/schoolView/editSchool/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { createContainer } from 'meteor/react-meteor-data';
+import { createContainer } from "meteor/react-meteor-data";
 import SchoolEditRender from "./schoolEditRender";
-import { browserHistory } from 'react-router';
+import { browserHistory } from "react-router";
 
 // import collection definition over here
 // import Modules from "/imports/api/modules/fields";
@@ -13,66 +13,79 @@ import SkillCategory from "/imports/api/skillCategory/fields";
 import SkillSubject from "/imports/api/skillSubject/fields";
 
 class SchoolEditView extends React.Component {
-
-	constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-    	selecetdView : "school_details",
+      selecetdView: "school_details",
       queryTabValue: null
     };
   }
   componentWillMount() {
     // Listen for `?classDetails=true` so that we can click on tab.
-    console.log("this.props.location.query.type != 'rejec",this.props.location.query.tabValue)
-    if(this.props.location.query.tabValue) {
+    console.log(
+      "this.props.location.query.type != 'rejec",
+      this.props.location.query.tabValue
+    );
+    if (this.props.location.query.tabValue) {
       // We should set state for class details tab so that it opens automatically.
-      this.setState({queryTabValue:this.props.location.query.tabValue})
+      this.setState({ queryTabValue: this.props.location.query.tabValue });
     }
   }
   checkSchoolAccess = (currentUser, schoolId) => {
-    if(!currentUser || !schoolId) {
-      browserHistory.push("/")
+    if (!currentUser || !schoolId) {
+      browserHistory.push("/");
+      return false;
+    } else if (checkMyAccess({ user: currentUser, schoolId })) {
+      return true;
+    } else if (
+      (currentUser.profile &&
+        currentUser.profile.schoolId &&
+        currentUser.profile.schoolId != schoolId) ||
+      !currentUser.profile ||
+      !currentUser.profile.schoolId
+    ) {
+      browserHistory.push("/");
       return false;
     }
-    else if(checkMyAccess({user: currentUser,schoolId})) {
-      return true
-    }
-    else if(currentUser.profile && currentUser.profile.schoolId && (currentUser.profile.schoolId != schoolId) || !currentUser.profile || !currentUser.profile.schoolId) {
-      browserHistory.push("/")
-      return false;
-    }
-  }
+  };
 
-  moveTab = (tabId) => {
+  moveTab = tabId => {
     this.refs[tabId] && this.refs[tabId].click();
-  }
-  moveToNextTab = (value) => {
-    this.setState({tabValue: value})
-  }
+  };
+  moveToNextTab = value => {
+    this.setState({ tabValue: value });
+  };
 
-  showFormBuilderModal = ({type, tableData, formFieldsValues, parentData}) => {
-    console.log("<<<< showEditModal >>>>>",parentData)
-    this.setState({
-      formBuilderModal: {
-        modalType: type,
-        tableData: tableData,
-        formFieldsValues: formFieldsValues,
-        parentData: parentData,
+  showFormBuilderModal = ({
+    type,
+    tableData,
+    formFieldsValues,
+    parentData
+  }) => {
+    console.log("<<<< showEditModal >>>>>", parentData);
+    this.setState(
+      {
+        formBuilderModal: {
+          modalType: type,
+          tableData: tableData,
+          formFieldsValues: formFieldsValues,
+          parentData: parentData
+        }
+      },
+      () => {
+        console.log("this.formBuilderModal show -->>", this.formBuilderModal);
+        this.formBuilderModal.show();
       }
-    }, ()=>{
-      console.log("this.formBuilderModal show -->>",this.formBuilderModal);
-      this.formBuilderModal.show();
-    })
-  }
+    );
+  };
 
-  onTabChange = (tabValue) => {
-    this.setState({tabValue})
-  }
+  onTabChange = tabValue => {
+    this.setState({ tabValue });
+  };
 
   render() {
-    return SchoolEditRender.call(this, this.props, this.state)
+    return SchoolEditRender.call(this, this.props, this.state);
   }
-
 }
 
 export default createContainer(props => {
@@ -93,8 +106,10 @@ export default createContainer(props => {
     // Meteor.subscribe("classType.getclassType", {schoolId});
   }
   let classTypeData;
-  let classTypeSubscription = Meteor.subscribe("classType.getclassType", { schoolId });
-  if (classTypeSubscription && classTypeSubscription.ready()) {
+  let classTypeSubscription = Meteor.subscribe("classType.getclassType", {
+    schoolId
+  });
+  if (classTypeSubscription) {
     isLoading = false;
     schoolData = School.findOne({ _id: schoolId });
     locationData = SLocation.find().fetch();
@@ -110,5 +125,4 @@ export default createContainer(props => {
     isLoading,
     classTypeData
   };
-
 }, SchoolEditView);
