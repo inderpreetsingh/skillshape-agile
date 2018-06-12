7d36da4ac6dd45a3451aa03f6e88c60a214e498c
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-06-12 18:27:02 +0530

    Two new buttons on classdetails for moveing back and forward

--- imports/ui/components/schoolView/editSchool/classTypeDetails/classTypeDetailsRender.js
+++ imports/ui/components/schoolView/editSchool/classTypeDetails/classTypeDetailsRender.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import PanelWithTable from '/imports/ui/componentHelpers/panelWithTable';
-import classTypeSettings from './classTypeSettings';
+import React from "react";
+import PanelWithTable from "/imports/ui/componentHelpers/panelWithTable";
+import classTypeSettings from "./classTypeSettings";
 import isEmpty from "lodash/isEmpty";
 
 export default function() {
+  let {
+    classTypeData,
+    showFormBuilderModal,
+    moveTab,
+    schoolId,
+    locationData
+  } = this.props;
 
-	let {
-		classTypeData,
-		showFormBuilderModal,
-		moveTab,
-		schoolId,
-		locationData,
-	} = this.props
-
-	return (
-		<div style={{paddingTop: '20px'}}>
-			<PanelWithTable
-				schoolId={schoolId}
-			    className="class-type-details"
-			    settings={classTypeSettings}
-			    mainTableData={classTypeData}
-			    getChildTableData={this.getChildTableData}
-			    showFormBuilderModal={showFormBuilderModal}
-			    locationData={locationData}
-			    handleImageChange={this.handleImageChange}
-			    handleImageSave={this.handleImageSave}
-			    showClassTypeModal={isEmpty(classTypeData)}
-			    moveToNextTab={this.moveToNextTab}
-			/>
-		</div>
-	)
+  return (
+    <div style={{ paddingTop: "20px" }}>
+      <PanelWithTable
+        schoolId={schoolId}
+        className="class-type-details"
+        settings={classTypeSettings}
+        mainTableData={classTypeData}
+        getChildTableData={this.getChildTableData}
+        showFormBuilderModal={showFormBuilderModal}
+        locationData={locationData}
+        handleImageChange={this.handleImageChange}
+        handleImageSave={this.handleImageSave}
+        showClassTypeModal={isEmpty(classTypeData)}
+        moveToNextTab={this.moveToNextTab}
+        moveToPreviousTab={this.moveToPreviousTab}
+      />
+    </div>
+  );
 }
