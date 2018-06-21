3c218c1e8d2e0830e1ae409b24940019f83b1990
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-06-21 19:13:25 +0530

    payout implemented in financials

--- imports/api/purchases/methods.js
+++ imports/api/purchases/methods.js
@@ -3,5 +3,9 @@ Meteor.methods({
   addPurchase: function(payload) {
     console.log("in purchase methods");
     Purchases.insert(payload);
+  },
+  getAllPurchaseData: function() {
+    let AllPurchaseData = Purchases.find().fetch();
+    return AllPurchaseData;
   }
 });
--- imports/ui/components/financials/payouts/index.js
+++ imports/ui/components/financials/payouts/index.js
@@ -1,14 +1,66 @@
 import React from "react";
+import Pagination from "/imports/ui/componentHelpers/pagination";
+import { PayoutDetailsTable } from "./payoutDetailsTable";
+import { TableRow, TableCell } from "material-ui/Table";
+import isEmpty from "lodash/isEmpty";
+import moment from "moment";
+const style = {
+  w211: {
+    width: 211
+  },
+  w100: {
+    width: 100
+  },
+  w150: {
+    width: 150
+  }
+};
 export default class Payouts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selecetdView: "Settings",
-      queryTabValue: null
+      PurchaseData: []
     };
   }
+  componentWillMount() {
+    Meteor.call("getAllPurchaseData", (err, res) => {
+      console.log("------------->AllPurchaseData<---------", res);
+      this.setState({ PurchaseData: res });
+    });
+  }
+  handlePageClick = ({ skip }) => {
+    console.log("skip -->>", skip);
+    this.setState({ isBusy: true });
+    this.getUsers({ limit: this.state.perPage, skip: skip });
+  };
 
   render() {
-    return <h1>Payouts</h1>;
+    const { PurchaseData } = this.state;
+    console.log("PurchaseData--->", PurchaseData);
+    return (
+      <div>
+        <h1>Payouts</h1>
+        <PayoutDetailsTable>
+          {isEmpty(PurchaseData)
+            ? "No payout found"
+            : PurchaseData.map(purchase => {
+                return (
+                  <TableRow key={purchase._id} selectable={false}>
+                    <TableCell style={style.w150}>
+                      {purchase.stripe_Response.amount || "Unavailable"}
+                    </TableCell>
+                    <TableCell style={style.w211}>
+                      {purchase.stripe_Response.source.last4 || "Unavailable"}
+                    </TableCell>
+                    <TableCell style={style.w150}>
+                      {moment(purchase.createdOn).format("YYYY-MM-DD") ||
+                        "Unavailable"}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+        </PayoutDetailsTable>
+      </div>
+    );
   }
 }
