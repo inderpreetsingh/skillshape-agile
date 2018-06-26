febfad4446499cdcb54741e6dd029e10e57691ac
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-06-26 16:46:09 +0530

    settings in financial page ui changed

--- imports/ui/components/financials/settings/index.js
+++ imports/ui/components/financials/settings/index.js
@@ -10,38 +10,42 @@ export default class Settings extends React.Component {
   }
 
   render() {
+    {
+      console.log("Meteor.user()", Meteor.user());
+    }
     return (
       <div>
         {" "}
-        <Card>
-          {" "}
-          <div style={{ height: "60px" }}>
-            <h3>
-              Connect your Stripe Account to SkillShape.<button
-                style={{
-                  backgroundColor: "#f44336",
-                  border: "none",
-                  color: "white",
-                  padding: "15px 32px",
-                  textAlign: "center",
-                  textDecoration: "none",
-                  display: "inline-block",
-                  fontSize: "16px",
-                  marginBottom: "5px",
-                  marginTop: "5px",
-                  float: "right"
-                }}
-                onClick={() => {
-                  location.href = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${
-                    Meteor.settings.public.stripeClientId
-                  }&scope=read_write`;
-                }}
-              >
-                Connect To Stripe
-              </button>
-            </h3>
-          </div>
-        </Card>
+        <center>
+          <Card>
+            {" "}
+            <div style={{ height: "60px" }}>
+              <h3>
+                Connect your Stripe Account to SkillShape.<button
+                  style={{
+                    backgroundColor: "#f44336",
+                    border: "none",
+                    color: "white",
+                    padding: "15px 32px",
+                    textAlign: "center",
+                    textDecoration: "none",
+                    display: "inline-block",
+                    fontSize: "16px",
+                    marginBottom: "5px",
+                    marginTop: "5px"
+                  }}
+                  onClick={() => {
+                    location.href = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${
+                      Meteor.settings.public.stripeClientId
+                    }&scope=read_write`;
+                  }}
+                >
+                  Connect To Stripe
+                </button>
+              </h3>
+            </div>
+          </Card>
+        </center>
       </div>
     );
   }
