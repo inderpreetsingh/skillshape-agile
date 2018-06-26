85842d0462917f663a35b5a1e81760e727259b51
ramesh-daffodil <ramesh.bansal@daffodilsw.com>
2018-06-26 13:38:39 +0530

    Fixes in claim school request

--- imports/api/claimSchoolRequest/methods.js
+++ imports/api/claimSchoolRequest/methods.js
@@ -1,120 +1,219 @@
-// import ClaimSchoolRequest from "./fields";
-// import { sendClaimASchoolEmail } from "/imports/api/email";
-// import { sendConfirmationEmail } from "/imports/api/email";
-// import School from "/imports/api/school/fields";
+import ClaimSchoolRequest from "./fields";
+import { sendClaimASchoolEmail } from "/imports/api/email";
+import { sendConfirmationEmail } from "/imports/api/email";
+import School from "/imports/api/school/fields";
+import { getUserFullName } from "/imports/util/getUserData";
 
-// Meteor.methods({
-//     "school.claimSchoolRequest": function(doc) {
-//         console.log("doc in claimSchoolRequest", doc);
-//         let currentUser = Meteor.users.findOne(doc.userId);
-//         let schoolData = School.findOne(doc.schoolId);
-//         console.log("schoolData", schoolData);
-//         // User's claim requests exists already in DB then need to send this message to user.
-//         let pendingClaimRequest = ClaimSchoolRequest.findOne({
-//             userId: doc.userId,
-//             status: "pending"
-//         });
-//         let rejectedClaimRequest = ClaimSchoolRequest.findOne({
-//             userId: doc.userId,
-//             status: "rejected",
-//             schoolId: doc.schoolId
-//         });
-//         console.log("pendingClaimRequest", pendingClaimRequest);
-//         console.log("rejectedClaimRequest", rejectedClaimRequest);
-//         // Claim request status is pending
-//         if (pendingClaimRequest) {
-//             if (pendingClaimRequest.schoolId == doc.schoolId) {
-//                 // User's request is already pending for this school
-//                 return { pendingRequest: true };
-//             } else {
-//                 // Users can not do more than one claim request.
-//                 return {
-//                     onlyOneRequestAllowed: true,
-//                     schoolName: pendingClaimRequest.schoolName
-//                 };
-//             }
-//         } else if (rejectedClaimRequest) {
-//             return { alreadyRejected: true };
-//         } else if (currentUser.profile && currentUser.profile.schoolId) {
-//             return { alreadyManage: true };
-//         }
-//         // No school email exists then just Make the user Admin of that school by System.
-//         if (!doc.schoolEmail) {
-//             let schoolEmail = currentUser.emails[0].address;
-//             let data = {};
-//             data.userId = this.userId;
-//             data.claimed = "Y";
-//             data.email = schoolEmail;
-//             doc.schoolEmail = schoolEmail;
-//             School.update({ _id: schoolData._id }, { $set: data });
-//             Meteor.users.update(
-//                 { _id: doc.userId },
-//                 {
-//                     $set: {
-//                         "profile.schoolId": doc.schoolId,
-//                         "profile.acess_type": "school"
-//                     }
-//                 }
-//             );
-//             doc.status = "approved";
-//             doc.approvedBy = "superadmin";
-//             doc.createdAt = new Date();
-//             ClaimSchoolRequest.insert(doc);
-//             return { message: "Claim request approved." };
-//         } else {
-//             doc.createdAt = new Date();
-//             doc.status = "pending";
-//             let claimRequestId = ClaimSchoolRequest.insert(doc);
-//             let schoolAdminUser = Meteor.users.findOne({
-//                 "emails.address": doc.schoolEmail
-//             });
-//             let manageBySelfUrl = `${Meteor.absoluteUrl()}?claimRequest=${claimRequestId}&schoolRegister=true`;
-//             /*User is school Admin in this case we need to send that user to school admin page if user login otherwise
-//             let him login and then redirect to school admin page.*/
-//             if (schoolAdminUser) {
-//                 manageBySelfUrl = `${Meteor.absoluteUrl()}?claimRequest=${claimRequestId}&redirectUrl=SchoolAdmin/${doc.schoolId}/edit&type=reject`;
-//             }
-//             let ROOT_URL = `${Meteor.absoluteUrl()}?claimRequest=${claimRequestId}`;
-//             let schoolAdminRec = Meteor.users.findOne(schoolData.userId);
-//             sendClaimASchoolEmail(
-//                 doc,
-//                 ROOT_URL,
-//                 manageBySelfUrl,
-//                 schoolAdminRec,
-//                 schoolData
-//             );
-//             sendConfirmationEmail(currentUser, schoolData);
-//             return {emailSuccess:true};
-//         }
-//     },
-//     approveSchoolClaimRequest: function(claimRequestId, status) {
-//         let claimRequestRec = ClaimSchoolRequest.findOne(claimRequestId);
-//         console.log("approveSchoolClaimRequest", claimRequestRec, status);
-//         if (claimRequestRec && claimRequestRec.status != "approved") {
-//             let data = {};
-//             data.userId = claimRequestRec.userId;
-//             data.claimed = "Y";
-//             data.email = claimRequestRec.schoolEmail;
-//             School.update({ _id: claimRequestRec.schoolId }, { $set: data });
-//             Meteor.users.update(
-//                 { _id: data.userId },
-//                 {
-//                     $set: {
-//                         "profile.schoolId": claimRequestRec.schoolId,
-//                         "profile.acess_type": "school"
-//                     }
-//                 }
-//             );
-//             ClaimSchoolRequest.update(
-//                 { _id: claimRequestId },
-//                 {
-//                     $set: {
-//                         status:
-//                             status && status.rejected ? "rejected" : "approved"
-//                     }
-//                 }
-//             );
-//             return true;
-//         }
-//     }
-// });
+Meteor.methods({
+  "claimSchoolRequest.createClaimSchoolRequest": function(doc) {
+    let currentUser = Meteor.users.findOne(doc.userId);
+    let schoolData = School.findOne(doc.schoolId);
+    // Check for PENDING OR REJECTED claim requests of current users if any and let them know the status of their request.
+    const pendingClaimRequest = ClaimSchoolRequest.findOne({
+      userId: doc.userId,
+      status: "pending",
+      schoolId: doc.schoolId
+    });
+    const rejectedClaimRequest = ClaimSchoolRequest.findOne({
+      userId: doc.userId,
+      status: "rejected",
+      schoolId: doc.schoolId
+    });
+    // Claim request status is pending OR rejected.
+    if (pendingClaimRequest) {
+      return { pendingRequest: true };
+    } else if (rejectedClaimRequest) {
+      return { alreadyRejected: true };
+    }
+    // No school email exists then just Make the user Admin of that school by System.
+    if (!schoolData.email && !schoolData.superAdmin) {
+      let data = {};
+      let schoolEmail = currentUser.emails && currentUser.emails[0].address;
+
+      data.admins = [this.userId];
+      data.superAdmin = this.userId;
+      data.claimed = "Y";
+      data.email = schoolEmail;
+      doc.schoolEmail = schoolEmail;
+      School.update({ _id: schoolData._id }, { $set: data });
+
+      let x = Meteor.users.update(
+        { _id: doc.userId },
+        {
+          $addToSet: {
+            "profile.schoolId": { $each: [doc.schoolId] }
+          }
+        }
+      );
+      console.log("xxxxxxxxxxxxxxx", x);
+      doc.status = "approved";
+      doc.approvedBy = "superadmin";
+      doc.createdAt = new Date();
+      ClaimSchoolRequest.insert(doc);
+      return { claimRequestApproved: true };
+    } else {
+      doc.userName = getUserFullName(currentUser);
+      doc.createdAt = new Date();
+      doc.status = "pending";
+      let claimRequestId = ClaimSchoolRequest.insert(doc);
+      let schoolAdminUser = Meteor.users.findOne({
+        "emails.address": doc.schoolEmail
+      });
+      let manageBySelfUrl = `${Meteor.absoluteUrl()}?claimRequest=${claimRequestId}&schoolRegister=true`;
+      /*User is school Admin in this case we need to send that user to school admin page if user login otherwise
+            let him login and then redirect to school admin page.*/
+      if (schoolAdminUser) {
+        manageBySelfUrl = `${Meteor.absoluteUrl()}?claimRequest=${claimRequestId}&redirectUrl=SchoolAdmin/${
+          doc.schoolId
+        }/edit&type=reject`;
+      }
+      let ROOT_URL = `${Meteor.absoluteUrl()}?claimRequest=${claimRequestId}`;
+      let schoolAdminRec = Meteor.users.findOne(schoolData.userId);
+
+      /** Needs to handle these cases:
+		1. Yes, make them an Admin, and keep me as SuperAdministrator.
+		2. Yes, make them SuperAdministrator and keep me as an Administrator.
+		3. Yes, make them SuperAdministrator and remove me as Administrator.
+		4. No, deny their request.
+		5. I have no idea what this is about.
+	*/
+      let modifyUsersRoles = {
+        keepMeSuperAdmin: `${ROOT_URL}&keepMeSuperAdmin=true`,
+        makeRequesterSuperAdmin: `${ROOT_URL}&makeRequesterSuperAdmin=true`,
+        removeMeAsAdmin: `${ROOT_URL}&removeMeAsAdmin=true`
+      };
+
+      sendClaimASchoolEmail(
+        doc,
+        ROOT_URL,
+        manageBySelfUrl,
+        schoolAdminRec,
+        schoolData,
+        modifyUsersRoles
+      );
+      sendConfirmationEmail(currentUser, schoolData);
+      return { emailSuccess: true };
+    }
+  },
+  "claimSchoolRequest.approveSchoolClaimRequest": function(
+    claimRequestId,
+    status
+  ) {
+    if (!this.userId) {
+      throw new Meteor.Error(
+        "You need to login as a super admin of this school to approve the request"
+      );
+    }
+    console.log("1111111111111111");
+    let claimRequestRec = ClaimSchoolRequest.findOne(claimRequestId);
+
+    /*
+             ***** `status` can be one of the following:****
+             * Keep Me SuperAdmin and Make Requester Admin.
+             * Keep me Admin and Make Requester Super Admin.
+             * Make Requester Super Admin and remove me admin.
+             * rejected.
+             * approved.
+         */
+    let schoolData = School.findOne(claimRequestRec.schoolId);
+    console.log(
+      "schoolData.superAdmin !== this.userId",
+      schoolData.superAdmin,
+      this.userId
+    );
+    if (schoolData.superAdmin !== this.userId) {
+      throw new Meteor.Error(
+        "You need to login as a super admin of this school to approve the request"
+      );
+    } else if (claimRequestRec.status == "approved") {
+      console.log("222222222222222");
+      throw new Meteor.Error("This request has already been approved!");
+    } else {
+      schoolData && delete schoolData._id;
+      let approveRequest = false;
+      if (status && status.keepMeSuperAdmin) {
+        // Just keep me as a Super Admin.
+        schoolData.superAdmin = this.userId;
+        // Make requester as an admin.
+        let existingAdmins =
+          schoolData.admins && schoolData.admins.length > 0
+            ? schoolData.admins
+            : [];
+        existingAdmins.push(claimRequestRec.userId);
+        schoolData.admins = existingAdmins;
+        approveRequest = true;
+      } else if (status && status.removeMeAsAdmin) {
+        approveRequest = true;
+        // Just Make Requester as a Super Admin and Remove me as an admin.
+        schoolData.superAdmin = claimRequestRec.userId;
+        if (claimRequestRec.userEmail) {
+          schoolData.email = claimRequestRec.userEmail;
+        }
+        // Remove me as an admin.
+        if (schoolData.admins && _.contains(schoolData.admins, this.userId)) {
+          let index = schoolData.admins.indexOf(this.userId);
+          let modifiedAdmins;
+          // Remove me as an admin so just pull my user id from `admins` of `School` rec
+          if (index > -1) {
+            modifiedAdmins = schoolData.admins.filter(function(item) {
+              return item !== schoolData.admins[index];
+            });
+          }
+          schoolData.admins = modifiedAdmins;
+          if (claimRequestRec.userEmail) {
+            schoolData.email = claimRequestRec.userEmail;
+          }
+        }
+      } else if (status && status.makeRequesterSuperAdmin) {
+        approveRequest = true;
+        // Make Requester Super Admin.
+        schoolData.superAdmin = claimRequestRec.userId;
+        // Make me as an admin.
+        let existingAdmins =
+          schoolData.admins && schoolData.admins.length > 0
+            ? schoolData.admins
+            : [];
+        existingAdmins.push(claimRequestRec.userId);
+        schoolData.admins = existingAdmins;
+      } else if (!status) {
+        // Just approve the request.
+        approveRequest = true;
+      }
+      // Approve Request case:
+      if (approveRequest) {
+        // Needs to update role and Email in `Users` rec.
+        School.update({ _id: claimRequestRec.schoolId }, { $set: schoolData });
+
+        Meteor.users.update(
+          { _id: claimRequestRec.userId },
+          {
+            $addToSet: {
+              "profile.schoolId": { $each: [claimRequestRec.schoolId] }
+            }
+          }
+        );
+
+        ClaimSchoolRequest.update(
+          { _id: claimRequestId },
+          {
+            $set: {
+              status: "approved"
+            }
+          }
+        );
+        return { message: "Request has been approved!" };
+      } else {
+        // Just reject the request.
+        ClaimSchoolRequest.update(
+          { _id: claimRequestId },
+          {
+            $set: {
+              status: "rejected"
+            }
+          }
+        );
+        return { message: "Request has been rejected!" };
+      }
+    }
+  }
+});
--- imports/ui/components/claimSchool/index.js
+++ imports/ui/components/claimSchool/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import {createContainer} from 'meteor/react-meteor-data';
-import Button from 'material-ui/Button';
-import { Link } from 'react-router';
-import Icon from 'material-ui/Icon';
+import React from "react";
+import { createContainer } from "meteor/react-meteor-data";
+import Button from "material-ui/Button";
+import { Link } from "react-router";
+import Icon from "material-ui/Icon";
 import { withStyles } from "material-ui/styles";
 
 import ClaimSchoolBase from "./claimSchoolBase";
 import ClaimSchoolRender from "./claimSchoolRender";
 import SkillCategory from "/imports/api/skillCategory/fields";
 
-import { toastrModal } from '/imports/util';
+import { toastrModal } from "/imports/util";
 
 const styles = theme => ({
   sideButton: {
     fontWeight: 600,
     borderRadius: 10,
-    backgroundColor: '#ca1e1e',
-    color: 'white',
+    backgroundColor: "#ca1e1e",
+    color: "white",
     marginRight: 18
   },
   textStyle: {
@@ -29,80 +29,94 @@ const styles = theme => ({
 // console.log("styles",styles);
 
 class ClaimSchool extends ClaimSchoolBase {
-
-    handleClaimASchool = (schoolCardData, confirmThis) => {
-        confirmThis.cancelConfirmationModal();
-        const { toastr} = this.props;
-        const user = Meteor.user();
-        const payload = {
-            schoolId: schoolCardData._id,
-            schoolName: schoolCardData.name,
-            userId: user._id,
-            userEmail: user.emails ? user.emails[0].address : user.services.google.email,
-            userName: user.profile.firstName || user.profile.name,
-            schoolEmail: schoolCardData.email
-        };
-        // Start loading
-        this.setState({ isLoading: true });
-        Meteor.call("school.claimSchoolRequest", payload, (err, result) => {
-            // Stop Loading
-            this.setState({ isLoading: false });
-            if(result) {
-                if (result.alreadyRejected) {
-                    toastr.error(
-                        "Your request has been rejected to manage this school by school Admin",
-                        "Error"
-                    );
-                } else if (result.pendingRequest) {
-                    toastr.error(
-                        "We are in the process of resolving your claim. We will contact you as soon as we reach a verdict or need more information. Thanks for your patience.",
-                        "Error"
-                    );
-                } else if (result.alreadyManage) {
-                    toastr.success(
-                        "You already manage a school. You cannot claim another School. Please contact admin for more details",
-                        "success"
-                    );
-                } else if (result.onlyOneRequestAllowed) {
-                    toastr.error(
-                        `You are not allowed to do more than one request as you have already created request for School Name:${result.schoolName}`,
-                        "Error"
-                    );
-                } else if (result.emailSuccess) {
-                    toastr.success(
-                        "We have sent your request to school admin. We will assist you soon :)",
-                        "success"
-                    );
-                } else if (result.claimRequestApproved) {
-                    toastr.success(
-                       `You have successfully claimed this school. Please click the button to manage your school!`,
-                       "success",
-                        {
-                            element: <div>
-                                <br></br>
-                                <Link style={{display: 'grid'}} target="_blank"  to={`${Meteor.absoluteUrl()}SchoolAdmin/${schoolCardData._id}/edit`}>
-                                    <Button raised color="accent">
-                                        Manage School <Icon>tag_faces</Icon>
-                                    </Button>
-                                </Link>
-                            </div>
-                        }
-                    );
-                }
-            }
-        });
+  handleClaimASchool = (schoolCardData, confirmThis) => {
+    confirmThis.cancelConfirmationModal();
+    const { toastr } = this.props;
+    const user = Meteor.user();
+    const payload = {
+      schoolId: schoolCardData._id,
+      schoolName: schoolCardData.name,
+      userId: user._id,
+      userEmail: user.emails
+        ? user.emails[0].address
+        : user.services.google.email,
+      userName: user.profile.firstName || user.profile.name,
+      schoolEmail: schoolCardData.email
     };
+    // Start loading
+    this.setState({ isLoading: true });
+    Meteor.call(
+      "claimSchoolRequest.createClaimSchoolRequest",
+      payload,
+      (err, result) => {
+        // Stop Loading
+        this.setState({ isLoading: false });
+        if (result) {
+          if (result.alreadyRejected) {
+            toastr.error(
+              "Your request has been rejected to manage this school by school Admin",
+              "Error"
+            );
+          } else if (result.pendingRequest) {
+            toastr.error(
+              "We are in the process of resolving your claim. We will contact you as soon as we reach a verdict or need more information. Thanks for your patience.",
+              "Error"
+            );
+          } else if (result.alreadyManage) {
+            toastr.success(
+              "You already manage a school. You cannot claim another School. Please contact admin for more details",
+              "success"
+            );
+          } else if (result.onlyOneRequestAllowed) {
+            toastr.error(
+              `You are not allowed to do more than one request as you have already created request for School Name:${
+                result.schoolName
+              }`,
+              "Error"
+            );
+          } else if (result.emailSuccess) {
+            toastr.success(
+              "We have sent your request to school admin. We will assist you soon :)",
+              "success"
+            );
+          } else if (result.claimRequestApproved) {
+            toastr.success(
+              `You have successfully claimed this school. Please click the button to manage your school!`,
+              "success",
+              {
+                element: (
+                  <div>
+                    <br />
+                    <Link
+                      style={{ display: "grid" }}
+                      target="_blank"
+                      to={`${Meteor.absoluteUrl()}SchoolAdmin/${
+                        schoolCardData._id
+                      }/edit`}
+                    >
+                      <Button raised color="accent">
+                        Manage School <Icon>tag_faces</Icon>
+                      </Button>
+                    </Link>
+                  </div>
+                )
+              }
+            );
+          }
+        }
+      }
+    );
+  };
 
-    render() {
-        // console.log("this.props claimSchoolRender",this)
-        return ClaimSchoolRender.call(this, this.props, this.state)
-    }
-
+  render() {
+    // console.log("this.props claimSchoolRender",this)
+    return ClaimSchoolRender.call(this, this.props, this.state);
+  }
 }
 
 export default createContainer(props => {
-    Meteor.subscribe('skillCategory.get');
-    let currentUser = Meteor.user();
-    let dataForSkillTypes = SkillCategory.find().fetch();
-    return {...props, dataForSkillTypes,currentUser};
-}, withStyles(styles,{ withTheme: true })(toastrModal(ClaimSchool)));
+  Meteor.subscribe("skillCategory.get");
+  let currentUser = Meteor.user();
+  let dataForSkillTypes = SkillCategory.find().fetch();
+  return { ...props, dataForSkillTypes, currentUser };
+}, withStyles(styles, { withTheme: true })(toastrModal(ClaimSchool)));
