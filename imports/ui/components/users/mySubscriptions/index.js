11c6d170e99d334a450b31be7a19021f2e3e7e5e
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-07-05 10:28:58 +0530

    conflic resolve

--- imports/ui/components/users/mySubscriptions/index.js
+++ imports/ui/components/users/mySubscriptions/index.js
@@ -12,14 +12,9 @@ export default class Mysubscriptions extends React.Component {
       <div>
         {this.props &&
           this.props.currentUser &&
-<<<<<<< db497c255fa6ab4959b1ebff05e717445dfa72a4
           this.props.currentUser._id &&
           Meteor.settings.public.paymentEnabled && (
             <SubscriptionDetails activeUserId={this.props.currentUser._id} />
-=======
-          this.props.currentUser._id && (
-            <SubscriptionDetails userId={this.props.currentUser._id} />
->>>>>>> email on expiration of package,table data from the purchases table
           )}
       </div>
     );
