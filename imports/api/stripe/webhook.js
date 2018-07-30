fc565a0286d3d4b3b94e18eda4f39a65e1201bdc
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-07-30 16:23:34 +0530

    comments added to be use in future

--- imports/api/stripe/webhook.js
+++ imports/api/stripe/webhook.js
@@ -2,7 +2,7 @@ var bodyParser = require("body-parser");
 Picker.middleware(bodyParser.json());
 Picker.middleware(bodyParser.urlencoded({ extended: false }));
 var dataFile = function(params, request, response, next) {
-  console.log(" request============", request.body);
+  console.log(" ============================================================", request.body);
   // var request = request.body;
   // console.log("request type", request.type);
   // switch (request.type) {
@@ -15,5 +15,17 @@ var dataFile = function(params, request, response, next) {
   // }
   // this.response.statusCode = 200;
   // this.response.end("");
+
+  /*
+  1.invoice.payment_succeeded
+  2.invoice.created
+  3.customer.subscription.created
+  4.customer.updated
+  5.invoice.created
+  6.invoice.upcoming
+  
+  
+  
+  */
 };
 Picker.route("/stripe-webhooks", dataFile);
