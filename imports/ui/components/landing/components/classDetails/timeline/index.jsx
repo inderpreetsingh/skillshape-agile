1467b4e4f6070c62b24227685966b386a918ba0f
Ishwerdas <singhs.ishwer@gmail.com>
2018-08-16 01:26:57 +0530

    updated date/time

--- imports/ui/components/landing/components/classDetails/timeline/index.jsx
+++ imports/ui/components/landing/components/classDetails/timeline/index.jsx
@@ -15,7 +15,7 @@ import * as helpers from "/imports/ui/components/landing/components/jss/helpers.
 const calculateElapsedTime = startTime => {
   // startTime is expected to be a date Obj with proper Time information stored in it
   if (startTime) {
-    const startTimeMoment = moment(new Date("2020-09-11"));
+    const startTimeMoment = moment(new Date());
     const currentTimeMoment = moment(new Date());
     return startTimeMoment.diff(currentTimeMoment, "minutes");
   }
