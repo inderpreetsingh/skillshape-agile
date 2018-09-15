22847eaa3120af3a9330954188eb5cece19d8117
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-09-15 10:40:39 +0530

    Every Url image will be compressed now  previously all fuctionality on client side but now partial functionality will be server side.

---
+++ imports/api/urlToBase64/methods.js
@@ -0,0 +1,9 @@
+
+import { HTTP } from 'meteor/http';
+Meteor.methods({
+    'urlToBase64.urlToBase64':(url)=>{
+        let response = HTTP.call('GET', url,{npmRequestOptions: { encoding: null }})
+        let data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(response.content).toString('base64');
+        return data;
+    }
+})
--- imports/util/compressImage.js
+++ imports/util/compressImage.js
@@ -1,9 +1,31 @@
 import imageCompression from 'browser-image-compression';
-srcToFile = async (src, fileName, mimeType) => {
-    let res = await fetch(src);
-    let buf = await res.arrayBuffer();
-    let final = await new File([buf], fileName, { type: mimeType });
-    return final;
+ base64ImageToBlob= async (str)=> {
+    // extract content type and base64 payload from original string
+    let pos = str.indexOf(';base64,');
+    let type = str.substring(5, pos);
+    let b64 = str.substr(pos + 8);
+    // decode base64
+    let imageContent = await atob(b64);
+    // create an ArrayBuffer and a view (as unsigned 8-bit)
+    let buffer =await new ArrayBuffer(imageContent.length);
+    let view = await new Uint8Array(buffer);
+    // fill the view, using the decoded base64
+    for(let n = 0; n < imageContent.length; n++) {
+      view[n] = imageContent.charCodeAt(n);
+    }
+    // convert ArrayBuffer to Blob
+    let blob = await new Blob([buffer], { type: type });
+    return blob;
+  }
+srcToFile =  (src, fileName, mimeType) => {
+   return new Promise((resolve,reject)=>{
+        Meteor.call('urlToBase64.urlToBase64',src,(err,res)=>{
+        base64ImageToBlob(res).then((blob)=>{
+        let file = new File([blob], fileName, {type: mimeType, lastModified: Date.now()});
+        resolve(file);
+        }) 
+        })
+    })
 }
 export const compressImage = async (file,url,useUrl) => {
     try {
@@ -19,7 +41,6 @@ export const compressImage = async (file,url,useUrl) => {
         }
         return compressedFiles;
     } catch (error) {
-    console.log('TCL: compress Image-> error', error);
         return error;
     }
 
