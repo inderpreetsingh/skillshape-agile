6c7114504c538ebb2e7165c55943ec6b9ddf17bc
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-10-30 10:47:44 +0530

    1. `skillshape-for-school` not opening because of missing components fixed. `Done`

--- imports/ui/components/landing/components/cards/SchoolSolutionCard.jsx
+++ imports/ui/components/landing/components/cards/SchoolSolutionCard.jsx
@@ -1,11 +1,8 @@
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import { lightenDarkenColor } from '/imports/util';
-import { Text } from '/imports/ui/components/landing/components/jss/sharedStyledComponents';
-import { cardImgSrc } from '/imports/ui/components/landing/site-settings.js';
 import * as helpers from '/imports/ui/components/landing/components/jss/helpers';
-
+import { Text } from '/imports/ui/components/landing/components/jss/sharedStyledComponents';
 const CardWrapper = styled.article`
   width: 100%;
   height: 100%;
