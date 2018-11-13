44da8becb16542da423db22a91f250852e591ee1
Ishwerdas <singhs.ishwer@gmail.com>
2018-11-13 20:11:55 +0530

    fixed school page link

--- imports/util/createTable.js
+++ imports/util/createTable.js
@@ -8,6 +8,12 @@ import Table, {
   TableRow
 } from 'material-ui/Table';
 
+const appendTableColumns = (tableRows) => {
+  return React.children.map(tableRows, (tableRow) => {
+    if (!React.isValidElement(tableRow)) return tableRow;
+  })
+
+}
 
 export const createTable = ({
   tableProps,
@@ -16,23 +22,23 @@ export const createTable = ({
   tableHeaderColumns,
   tableHeaderColumnProps,
   tableBodyProps,
-  }) => ({ children: tableRows, ...props }) => (
-    <Table
-      {...tableProps}
-      {...props}
-      bodyStyle={{overflow:'visible'}}
-    >
-      <TableHead {...tableHeaderProps}>
-        <TableRow {...tableRowProps}>
-          {
-            tableHeaderColumns && Array.isArray(tableHeaderColumns) && tableHeaderColumns.map(({ tooltip, columnName, className }, id) => (
-              <TableCell key={id} className={className}  tooltip={tooltip} {...tableHeaderColumnProps}>{columnName}</TableCell>
-            ))
-          }
-        </TableRow>
-      </TableHead>
-      <TableBody {...tableBodyProps}>
-        {tableRows}
-      </TableBody>
-    </Table>
-  )
+}) => ({ children: tableRows, ...props }) => (
+  <Table
+    {...tableProps}
+    {...props}
+    bodyStyle={{ overflow: 'visible' }}
+  >
+    <TableHead {...tableHeaderProps}>
+      <TableRow {...tableRowProps}>
+        {
+          tableHeaderColumns && Array.isArray(tableHeaderColumns) && tableHeaderColumns.map(({ tooltip, columnName, className }, id) => (
+            <TableCell key={id} className={className} tooltip={tooltip} {...tableHeaderColumnProps}>{columnName}</TableCell>
+          ))
+        }
+      </TableRow>
+    </TableHead>
+    <TableBody {...tableBodyProps}>
+      {tableRows}
+    </TableBody>
+  </Table>
+)
