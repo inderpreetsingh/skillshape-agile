67c85cf8545fe6a7f6306ae318335cfe4e7c8049
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2018-11-27 14:17:56 +0530

    6.  Direct opening of the page of class details page handling. `Done`

--- imports/ui/components/landing/ClassDetails.jsx
+++ imports/ui/components/landing/ClassDetails.jsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import ClassDetails from "/imports/ui/components/landing/components/classDetails/index.jsx";
 import { get, isEmpty, includes, remove } from 'lodash';
 import { createContainer } from 'meteor/react-meteor-data';
+import NoPageFound from "/imports/ui/components/landing/components/NoPageFound";
 
 import { classModulesData, classTimeData } from "/imports/ui/components/landing/constants/classDetails/";
 import { withPopUp } from '/imports/util';
 import Classes from "/imports/api/classes/fields";
 import ClassTime from '/imports/api/classTimes/fields.js';
+import { browserHistory } from "react-router";
 
 import { coverSrc, classTypeImgSrc } from "/imports/ui/components/landing/site-settings.js";
 
@@ -27,6 +29,12 @@ class ClassDetailsContainer extends Component {
 
   render() {
     const { currentUser, isUserSubsReady, classData, instructorsData, popUp, instructorsIds } = this.props;
+    if(isEmpty(classData) && isEmpty(instructorsData) && isEmpty(instructorsIds)){
+      return (
+        <NoPageFound/>
+      );
+    }
+    
     return (
       <ClassDetails
         topSearchBarProps={{
@@ -53,23 +61,37 @@ class ClassDetailsContainer extends Component {
 }
 
 export default createContainer((props) => {
-  const { state } = props.location.state;
-  const dataProps = props.location.state.props;
-  let schoolId, classTypeId, classTimeId, scheduled_date, classesSubscription, classData, instructorsIds,
+  let schoolId, classTypeId, classTimeId, scheduled_date, classesSubscription, classData=[], instructorsIds=[],
     instructorsData = [], userSubscription, classTimeSubscription, ClassTimeData;
-  schoolId = state.school._id;
-  classTimeId = state.eventData.classTimeId;
-  classTypeId = state.eventData.classTypeId;
-  filter = { _id: state.classDetails._id };
-  classesSubscription = Meteor.subscribe('classes.getClassesData', filter);
-  if (classesSubscription && classesSubscription.ready()) {
-    classData = Classes.find().fetch();
-    instructorsIds = get(classData[0], 'instructors', []);
-    if (isEmpty(instructorsIds)) {
-      classTimeSubscription = Meteor.subscribe('classTimes.getclassTimes', { schoolId, classTypeId });
-      if (classTimeSubscription && classTimeSubscription.ready()) {
-        ClassTimeData = ClassTime.find().fetch();
-        instructorsIds = get(ClassTimeData[0], 'instructors', []);
+  const state  = get(props.location.state,'state',null);
+  const dataProps = get(props.location.state,'props',null);
+  if(state && dataProps){
+    schoolId = state.school._id;
+    classTimeId = state.eventData.classTimeId;
+    classTypeId = state.eventData.classTypeId;
+    filter = { _id: state.classDetails._id };
+    classesSubscription = Meteor.subscribe('classes.getClassesData', filter);
+    if (classesSubscription && classesSubscription.ready()) {
+      classData = Classes.find().fetch();
+      instructorsIds = get(classData[0], 'instructors', []);
+      if (isEmpty(instructorsIds)) {
+        classTimeSubscription = Meteor.subscribe('classTimes.getclassTimes', { schoolId, classTypeId });
+        if (classTimeSubscription && classTimeSubscription.ready()) {
+          ClassTimeData = ClassTime.find().fetch();
+          instructorsIds = get(ClassTimeData[0], 'instructors', []);
+          userSubscription = Meteor.subscribe('user.getUsersFromIds', instructorsIds);
+          if (userSubscription && userSubscription.ready()) {
+            instructorsData = Meteor.users.find().fetch();
+            if (!includes(instructorsIds, Meteor.userId())) {
+              instructorsData = remove(instructorsData, (ele) => {
+                return ele._id != Meteor.userId()
+              })
+            }
+          }
+        }
+  
+      }
+      else if (!isEmpty(instructorsIds)) {
         userSubscription = Meteor.subscribe('user.getUsersFromIds', instructorsIds);
         if (userSubscription && userSubscription.ready()) {
           instructorsData = Meteor.users.find().fetch();
@@ -80,20 +102,8 @@ export default createContainer((props) => {
           }
         }
       }
-
+  
     }
-    else if (!isEmpty(instructorsIds)) {
-      userSubscription = Meteor.subscribe('user.getUsersFromIds', instructorsIds);
-      if (userSubscription && userSubscription.ready()) {
-        instructorsData = Meteor.users.find().fetch();
-        if (!includes(instructorsIds, Meteor.userId())) {
-          instructorsData = remove(instructorsData, (ele) => {
-            return ele._id != Meteor.userId()
-          })
-        }
-      }
-    }
-
   }
   return {
     classData,
