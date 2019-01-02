e1786026b603a29c07170606d88221654c30a245
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-01-02 14:31:44 +0530

    4.  This flow implemented. `Done` ```if a monthly unlimited package for that class type, just confirm that it is covered by that package: "This class is covered by {limited monthly or perclass package name}" [Cancel] [Confirm Sign In]```

---
+++ imports/util/confirmationDialog.js
@@ -0,0 +1,24 @@
+import FormGhostButton from '/imports/ui/components/landing/components/buttons/FormGhostButton.jsx';
+import styled from "styled-components";
+import { rhythmDiv } from '/imports/ui/components/landing/components/jss/helpers.js';
+import React from "react";
+const ButtonWrapper = styled.div`margin-bottom: ${rhythmDiv}px;`;
+
+export function confirmationDialog(data) {
+   console.log("​confirmationDialog -> data", data)
+   const {title,content,buttons,type,popUp} = data;
+   popUp.appear(type, {
+    title,
+    content,
+    RenderActions: (<ButtonWrapper>
+      {buttons.map((obj)=>{
+          return ( 
+              <FormGhostButton
+                  applyClose
+                  {...obj}
+              />
+          )
+      })}
+  </ButtonWrapper>)
+  }, true);
+  }
