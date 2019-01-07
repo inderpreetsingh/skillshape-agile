374cfd401f840cdfd390311db422af0b56ff82ec
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-01-07 18:23:58 +0530

    UI Improved

--- imports/ui/components/users/myTransaction/filterCode.js
+++ imports/ui/components/users/myTransaction/filterCode.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { Fragment} from "react";
+import React, { Fragment } from "react";
 import ReactSelect from "react-select";
 import Grid from "material-ui/Grid";
 const Wrapper = styled.div`
@@ -8,38 +8,40 @@ const Wrapper = styled.div`
 const customStyle = {
     marginTop: "10px",
     marginBottom: '10px',
-  }
+}
 export const filterForTransaction = function () {
-    const {selectedPackageType,packageTypeOptions,selectedPackageStatus,packageStatusOptions} = this.state;
+    const { selectedPackageType, packageTypeOptions, selectedPackageStatus, packageStatusOptions } = this.state;
+    const {classes} = this.props;
     return (
         <Wrapper>
-            <Grid container spacing={24}>
-        <Grid item xs={6} sm={3}>
-        <ReactSelect
-                    name="filters"
-                    style={customStyle}
-                    placeholder={<b>Package Type</b>}
-                    value={selectedPackageType}
-                    options={packageTypeOptions}
-                    onChange={(data)=>{this.handleFilter(data,'packageType','selectedPackageType')}}
-                    closeMenuOnSelect={false}
-                    clearable={false}
-                  />
-        </Grid>
-        <Grid item xs={6} sm={3}>
-        <ReactSelect
-                    name="filters"
-                    style={customStyle}
-                    placeholder={<b>Package Status</b>}
-                    value={selectedPackageStatus}
-                    options={packageStatusOptions}
-                    onChange={(data)=>{this.handleFilter(data,'packageStatus','selectedPackageStatus')}}
-                    closeMenuOnSelect={false}
-                    clearable={false}
-                  />
-        </Grid>
-        </Grid>
-           
+            <center>
+                <Grid container spacing={24} className={classes.rootGrid}>
+                    <Grid item xs={6} sm={3}>
+                        <ReactSelect
+                            name="filters"
+                            style={customStyle}
+                            placeholder={<b>Package Type</b>}
+                            value={selectedPackageType}
+                            options={packageTypeOptions}
+                            onChange={(data) => { this.handleFilter(data, 'packageType', 'selectedPackageType') }}
+                            closeMenuOnSelect={false}
+                            clearable={false}
+                        />
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                        <ReactSelect
+                            name="filters"
+                            style={customStyle}
+                            placeholder={<b>Package Status</b>}
+                            value={selectedPackageStatus}
+                            options={packageStatusOptions}
+                            onChange={(data) => { this.handleFilter(data, 'packageStatus', 'selectedPackageStatus') }}
+                            closeMenuOnSelect={false}
+                            clearable={false}
+                        />
+                    </Grid>
+                </Grid>
+            </center>
         </Wrapper>
 
     );
--- imports/ui/components/users/myTransaction/index.js
+++ imports/ui/components/users/myTransaction/index.js
@@ -1,84 +1,99 @@
 import React, { Fragment } from "react";
-import {isEmpty,get} from "lodash";
+import { isEmpty, get } from "lodash";
 import Pagination from "/imports/ui/componentHelpers/pagination";
 import { TransactionDetailsTable, getTableProps } from "./transactionDetailsTable";
-import { dateFriendly ,capitalizeString} from "/imports/util";
+import { dateFriendly, capitalizeString } from "/imports/util";
 import { FncTableCell, FncTableRow } from './styles';
 import { ContainerLoader } from "/imports/ui/loading/container";
-import {filterForTransaction} from './filterCode';
-const packageTypes = [{label:'Package Type All',value:0},{label:"CP",value:"CP"},{label:"MP",value:'MP'},{label:"EP",value:'EP'}]
-const packageStatus = [{label:'Package Status All',value:0},{label:'Active',value:'active'},{label:'Expired',value:'expired'},{label:'In Active',value:'inActive'}]
-export default class MyTransaction extends React.Component {
+import { filterForTransaction } from './filterCode';
+import { withStyles } from "material-ui/styles";
+import Paper from 'material-ui/Paper'
+const packageTypes = [{ label: 'Package Type All', value: 0 }, { label: "CP", value: "CP" }, { label: "MP", value: 'MP' }, { label: "EP", value: 'EP' }]
+const packageStatus = [{ label: 'Package Status All', value: 0 }, { label: 'Active', value: 'active' }, { label: 'Expired', value: 'expired' }, { label: 'In Active', value: 'inActive' }]
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    backgroundColor: "#4caf50",
+    margin: "10px",
+    borderRadius: "10px"
+  },
+rootGrid:{
+  margin:"0px"
+}
+})
+class MyTransaction extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       purchaseData: [],
       perPage: 3,
-      pageCount:1,
-      selectedPackageType:null,
-      selectedPackageStatus:null,
-      packageStatusOptions:packageStatus,
-      packageTypeOptions:packageTypes,
-      filter:{
-       userId:Meteor.userId()
+      pageCount: 1,
+      selectedPackageType: null,
+      selectedPackageStatus: null,
+      packageStatusOptions: packageStatus,
+      packageTypeOptions: packageTypes,
+      filter: {
+        userId: Meteor.userId()
       },
-      limit:3,
-      skip:0,
-      isLoading:false
+      limit: 3,
+      skip: 0,
+      isLoading: false
     };
   }
- 
+
   componentWillMount() {
     this.getPurchaseData();
   }
   // get purchase data from db on page load and page number click.
-  getPurchaseData = () =>{
-    this.setState({isLoading:true});
-    let {filter} = this.state;
-    let {limit,skip} = this.state;
-    let limitAndSkip = {limit,skip};
-    Meteor.call('purchases.getFilteredPurchases',filter,limitAndSkip,(err,res)=>{
+  getPurchaseData = () => {
+    this.setState({ isLoading: true });
+    let { filter } = this.state;
+    let { limit, skip } = this.state;
+    let limitAndSkip = { limit, skip };
+    Meteor.call('purchases.getFilteredPurchases', filter, limitAndSkip, (err, res) => {
       let state = {};
-      if(res){
-        state = {pageCount:Math.ceil(res.count/3),purchaseData:res.records}  
+      if (res) {
+        state = { pageCount: Math.ceil(res.count / 3), purchaseData: res.records }
         state.isLoading = false;
-        this.setState(state); 
+        this.setState(state);
       }
     })
   }
-  changePageClick = (skip) =>{
-    this.setState({skip:skip.skip,isLoading:true},()=>{this.getPurchaseData();});
+  changePageClick = (skip) => {
+    this.setState({ skip: skip.skip, isLoading: true }, () => { this.getPurchaseData(); });
   }
-  getColumnValue = (data,fieldName) =>{
-   let result = get(data,fieldName,'Missing');
-   if(typeof result == "string"){
-     return capitalizeString(result)
-   }
-   return result;
+  getColumnValue = (data, fieldName) => {
+    let result = get(data, fieldName, 'Missing');
+    if (typeof result == "string") {
+      return capitalizeString(result)
+    }
+    return result;
   }
-  packageType = (purchase) =>{
-    let pt = get(purchase,'packageType','Unavailable');
-    pt == 'MP' ? pt = 'Monthly Package' : pt == 'CP' ? pt='Per Class':pt == 'EP' ? pt ='Enrollment Package' : pt='Unavailable'; 
+  packageType = (purchase) => {
+    let pt = get(purchase, 'packageType', 'Unavailable');
+    pt == 'MP' ? pt = 'Monthly Package' : pt == 'CP' ? pt = 'Per Class' : pt == 'EP' ? pt = 'Enrollment Package' : pt = 'Unavailable';
     return capitalizeString(pt);
   }
-  handleFilter = (value,filterName,stateName) => {
+  handleFilter = (value, filterName, stateName) => {
     this.setState(state => {
-      let {filter} = state;
-      if(value.value && filterName)
-      filter[filterName] = value.value;
+      let { filter } = state;
+      if (value.value && filterName)
+        filter[filterName] = value.value;
       else
-      delete filter[filterName];
+        delete filter[filterName];
       return {
         [stateName]: value,
         filter,
-        isLoading:true,
-        skip:0,
+        isLoading: true,
+        skip: 0,
       };
-    },()=>{this.getPurchaseData();});
+    }, () => { this.getPurchaseData(); });
   };
   render() {
     const { tableHeaderColumns } = getTableProps();
-   const { purchaseData,isLoading }= this.state;
+    const { purchaseData, isLoading } = this.state;
+    const { classes } = this.props;
     return (
       <div>
         {isLoading && <ContainerLoader />}
@@ -86,7 +101,9 @@ export default class MyTransaction extends React.Component {
           {" "}
           <h1>My Transactions </h1>
         </center>
+        <Paper className={classes.root}>
         {filterForTransaction.call(this)}
+        </Paper>
         <TransactionDetailsTable>
           {isEmpty(purchaseData)
             ? "No payout found"
@@ -94,35 +111,35 @@ export default class MyTransaction extends React.Component {
               return (
                 <Fragment>
                   <FncTableRow key={index} selectable={false}>
-                  <FncTableCell data-th={tableHeaderColumns[0].columnName}>
-                       {index+1} 
+                    <FncTableCell data-th={tableHeaderColumns[0].columnName}>
+                      {index + 1}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[1].columnName}>
-                       {this.getColumnValue(purchase,'userName')} 
+                      {this.getColumnValue(purchase, 'userName')}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[2].columnName}>
-                      {this.getColumnValue(purchase,'packageName')}
+                      {this.getColumnValue(purchase, 'packageName')}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[3].columnName}>
                       {this.packageType(purchase)}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[4].columnName}>
-                      {this.getColumnValue(purchase,'amount')}
+                      {this.getColumnValue(purchase, 'amount')}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[5].columnName}>
-                      {dateFriendly(this.getColumnValue(purchase,'startDate'),"MMMM Do YYYY, h:mm:ss a")}
+                      {dateFriendly(this.getColumnValue(purchase, 'startDate'), "MMMM Do YYYY, h:mm:ss a")}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[6].columnName}>
-                      {this.getColumnValue(purchase,'paymentMethod')}
+                      {this.getColumnValue(purchase, 'paymentMethod')}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[7].columnName}>
-                    {dateFriendly(this.getColumnValue(purchase,'endDate'),"MMMM Do YYYY, h:mm:ss a")}
+                      {dateFriendly(this.getColumnValue(purchase, 'endDate'), "MMMM Do YYYY, h:mm:ss a")}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[8].columnName}>
-                      { this.getColumnValue(purchase,'noClasses') == null ? 'Unlimited' : this.getColumnValue(purchase,'noClasses') }
+                      {this.getColumnValue(purchase, 'noClasses') == null ? 'Unlimited' : this.getColumnValue(purchase, 'noClasses')}
                     </FncTableCell>
                     <FncTableCell data-th={tableHeaderColumns[9].columnName}>
-                      {this.getColumnValue(purchase,'packageStatus')}
+                      {this.getColumnValue(purchase, 'packageStatus')}
                     </FncTableCell>
                   </FncTableRow>
                 </Fragment>
@@ -137,3 +154,4 @@ export default class MyTransaction extends React.Component {
     );
   }
 }
+export default withStyles(styles)(MyTransaction);
