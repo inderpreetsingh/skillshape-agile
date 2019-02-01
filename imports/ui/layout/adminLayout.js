30076a4a5634d70b669ca066e79d5baf707ca5e1
Ishwerdas <singhs.ishwer@gmail.com>
2019-02-01 13:03:48 +0530

    improved the media frontend issues [edit-school-page]

--- imports/ui/layout/adminLayout.js
+++ imports/ui/layout/adminLayout.js
@@ -9,37 +9,37 @@ import { withStyles, material_ui_next_theme } from '/imports/util';
 import muiTheme from '/imports/ui/components/landing/components/jss/muitheme.jsx';
 
 const styles = theme => ({
-    wrapper : {
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    content: {
-        backgroundColor: theme.palette.background.default,
-        paddingTop: theme.spacing.unit*10,
-        overflow: 'hidden',
-    },
+  wrapper: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    paddingTop: theme.spacing.unit * 10,
+    overflow: 'hidden',
+  },
 });
 
 class AdminLayout extends React.Component {
 
-  constructor( props ) {
-    super( props );
+  constructor(props) {
+    super(props);
   }
 
   getMainPanelRef() {
     return this.mainPanelRef
   }
 
-  render( ) {
+  render() {
     // console.log("Admin layout props -->>",this.props);
-    const { currentUser, classes, isUserSubsReady} = this.props;
+    const { currentUser, classes, isUserSubsReady } = this.props;
     let className = {
       mainClass: "wrapper perfectScroll main_wrapper",
       contentClass: "content",
       id: "UserMainPanel",
     }
-    if(currentUser) {
+    if (currentUser) {
       className.mainClass = "main-panel";
       className.contentClass = "content no-padding";
       className.id = "UserMainPanel";
@@ -47,12 +47,12 @@ class AdminLayout extends React.Component {
     return (
       <MuiThemeProvider theme={muiTheme}>
         <div className={`${className.mainClass} ${classes.wrapper}`} id={className.id}>
-            <BrandBar {...this.props}/>
-            <div style={{flex: 1}} ref={(ref)=> {this.mainPanelRef = ref}}>
-               <main className={classes.content}>
-                {React.cloneElement(this.props.children, { currentUser: currentUser, isUserSubsReady: isUserSubsReady })}
-               </main>
-            </div>
+          <BrandBar {...this.props} />
+          <div style={{ flex: 1 }} ref={(ref) => { this.mainPanelRef = ref }}>
+            <main id="ss-main" className={classes.content}>
+              {React.cloneElement(this.props.children, { currentUser: currentUser, isUserSubsReady: isUserSubsReady })}
+            </main>
+          </div>
           <Footer />
         </div>
       </MuiThemeProvider>
