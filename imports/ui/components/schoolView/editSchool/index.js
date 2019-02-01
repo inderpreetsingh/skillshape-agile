0de6c6d2ffde2a9a1a19fde8c8a7493709df311b
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-02-01 12:07:44 +0530

    3. Lazy loading added on the school edit page for a smooth experience. `Done`

--- imports/ui/components/schoolView/editSchool/index.js
+++ imports/ui/components/schoolView/editSchool/index.js
@@ -7,6 +7,8 @@ import School from "/imports/api/school/fields";
 import SLocation from "/imports/api/sLocation/fields";
 import config from "/imports/config";
 import get from 'lodash/get';
+import { ContainerLoader } from "/imports/ui/loading/container";
+
 class SchoolEditView extends React.Component {
   constructor(props) {
     super(props);
@@ -91,47 +93,47 @@ class SchoolEditView extends React.Component {
   onTabChange = tabValue => {
     this.setState({ tabValue });
   };
-
+  shouldComponentUpdate(nextProps){
+    return !nextProps.isLoading;
+  }
   render() {
+    console.count('school edit 4')
+    if(this.props.isLoading){
+      return <ContainerLoader/>
+    }
     return SchoolEditRender.call(this, this.props, this.state);
   }
 }
 
 export default createContainer(props => {
-  let { schoolId,slug } = props.params;
-  let subscription;
+  let { schoolId, slug } = props.params;
   let schoolData;
   let locationData;
   let moduleData;
   let isLoading = true;
   let currency;
-if(slug && !schoolId){
- subscription = Meteor.subscribe("schoolMemberDetails.getSchoolMemberWithSchool", {slug});
- if(subscription && subscription.ready()){
-   schoolId = School.find({ slug: slug }).fetch()[0]._id;
- }
-}
-  if (props.isUserSubsReady) {
-    subscription = Meteor.subscribe("UserSchool", schoolId);
-    Meteor.subscribe("location.getSchoolLocation", { schoolId });
-    Meteor.subscribe("classtype");
-    Meteor.subscribe("SkillType");
-    Meteor.subscribe("SkillClassbySchool", schoolId);
-    // Meteor.subscribe("modules.getModules", {schoolId});
-    // Meteor.subscribe("classType.getclassType", {schoolId});
+  let userSchoolSub, locationSub, classTypeSub, skillClassSchoolSub,
+    schoolMemberDetailsSub, classTypeData;
+  if (slug && !schoolId) {
+    schoolMemberDetailsSub = Meteor.subscribe("schoolMemberDetails.getSchoolMemberWithSchool", { slug });
+    if (schoolMemberDetailsSub && schoolMemberDetailsSub.ready()) {
+      schoolId = School.find({ slug: slug }).fetch()[0]._id;
+    }
   }
-  let classTypeData;
-  Meteor.subscribe("classType.getclassType", {
-    schoolId
-  });
-  if (subscription && subscription.ready()) {
-    isLoading = false;
-    schoolData = School.findOne({ _id: schoolId });
-    currency = schoolData && schoolData.currency ? schoolData.currency : config.defaultCurrency;
-    locationData = SLocation.find().fetch();
-    classTypeData = ClassType.find({ schoolId: schoolId }).fetch();
+  if (schoolId) {
+    userSchoolSub = Meteor.subscribe("UserSchool", schoolId);
+    locationSub = Meteor.subscribe("location.getSchoolLocation", { schoolId });
+    skillClassSchoolSub = Meteor.subscribe("SkillClassbySchool", schoolId);
+    classTypeSub = Meteor.subscribe("classType.getclassType", { schoolId });
+    let isAllSubsReady = userSchoolSub && userSchoolSub.ready() && locationSub && locationSub.ready() && skillClassSchoolSub && skillClassSchoolSub.ready() && classTypeSub && classTypeSub.ready();
+    if (props.isUserSubsReady && isAllSubsReady) {
+      isLoading = false;
+      schoolData = School.findOne({ _id: schoolId });
+      currency = schoolData && schoolData.currency ? schoolData.currency : config.defaultCurrency;
+      locationData = SLocation.find().fetch();
+      classTypeData = ClassType.find({ schoolId: schoolId }).fetch();
+    }
   }
-
   return {
     ...props,
     schoolId,
