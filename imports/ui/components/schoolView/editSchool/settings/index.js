6b1fadb77c9d3abf14f180afc21895653fa79e78
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-02-12 18:20:04 +0530

    9. Remove Financial and put Transactions and Settings as top level menu items. `Done`

---
+++ imports/ui/components/schoolView/editSchool/settings/index.js
@@ -0,0 +1,108 @@
+import React from "react";
+import Card from "material-ui/Card";
+import { withPopUp } from "/imports/util";
+import get from 'lodash/get';
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selecetdView: "Settings",
+      queryTabValue: null,
+      status: false
+    };
+  }
+
+  disconnectStripe = () => {
+    const { popUp } = this.props;
+    let {superAdmin} = this.state;
+    if(superAdmin==Meteor.userId()){
+      Meteor.call("stripe.disconnectStripeUser", superAdmin,(error, result) => {
+        this.setState({status:false});
+        popUp.appear("success", { title: "Success", content: result});
+      });
+    }else{
+      popUp.appear("alert", { title: "Error", content: "Only super admin of school can control stripe."});
+    }
+  };
+  
+  componentWillMount() {
+    const {schoolData} = this.props;
+    let superAdmin = get(schoolData,'superAdmin',null)
+    Meteor.call("stripe.findAdminStripeAccount",superAdmin,(err,res)=>{
+      this.setState({status:res,superAdmin});      
+    })
+  }
+  
+  render() {
+    const {status,superAdmin} = this.state; 
+    const {popUp} = this.props;
+    return (
+      <div>
+            <div>
+              {status ? (
+                <center>
+                  <Card>
+                    {" "}
+                    <div style={{ height: "60px" }}>
+                      <h3>
+                        Already Connected to Stripe.<button
+                          style={{
+                            backgroundColor: "#4CAF50",
+                            border: "none",
+                            color: "white",
+                            padding: "9px 21px",
+                            textAlign: "center",
+                            textDecoration: "none",
+                            display: "inline-block",
+                            fontSize: "16px",
+                            borderRadius:'17px'
+                          }}
+                          onClick={this.disconnectStripe}
+                        >
+                          Disconnect Stripe
+                        </button>
+                      </h3>
+                    </div>
+                  </Card>
+                </center>
+              ) : (
+                <center>
+                  <Card>
+                    {" "}
+                    <div style={{ height: "60px" }}>
+                      <h3>
+                        Connect your Stripe Account to SkillShape.<button
+                          style={{
+                            backgroundColor: "#f44336",
+                            border: "none",
+                            color: "white",
+                            padding: "9px 21px",
+                            textAlign: "center",
+                            textDecoration: "none",
+                            display: "inline-block",
+                            fontSize: "16px",
+                            borderRadius:'17px'
+                          }}
+                          onClick={() => {
+                            if(superAdmin==Meteor.userId()){
+                              location.href = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${ Meteor.settings.public.stripeClientId }&scope=read_write&redirect_uri=${Meteor.absoluteUrl()}redirect-to-stripe`;
+                            }
+                            else{
+                              popUp.appear("alert", { title: "Error", content: "Only super admin of school can control stripe."});
+                            }
+                          }}
+                        >
+                          Connect Stripe
+                        </button>
+                      </h3>
+                    </div>
+                  </Card>
+                </center>
+              )}
+            </div>
+          
+      </div>
+    );
+  }
+}
+export default withPopUp(Settings);
