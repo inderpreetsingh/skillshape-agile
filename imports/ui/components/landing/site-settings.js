4bba85f66f7515160eac322a315f195160667919
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-03-18 16:50:50 +0530

    fix aws issue

--- imports/ui/components/landing/site-settings.js
+++ imports/ui/components/landing/site-settings.js
@@ -1,6 +1,6 @@
 export const logoSrc = "/images/landing/logo.png",
   coverSrc =
-    "https://s3-us-west-1.amazonaws.com/skillshape/schools/e537fa24-3624-4cf8-9765-59e4aed9d7e0.jpg",
+    "https://s3.eu-central-1.amazonaws.com/sslive/compressed/covergb.jpg",
   cardImgSrc = "/images/classtype/classtype-cover.jpg",
   classTypeImgSrc = "/images/classtype/classtype-cover.jpg",
   schoolDetailsImgSrc = "/images/classtype/classtype-cover.jpg",
