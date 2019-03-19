ff43f4b2adc84ba14657e1e592321afd22cf462c
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-03-19 15:46:50 +0530

    4. Transactions added and Design CSS used by inder. `Done`

--- imports/ui/pages/emailVerifyDashboard/emailVerifyDashboardRender.jsx
+++ imports/ui/pages/emailVerifyDashboard/emailVerifyDashboardRender.jsx
@@ -1,17 +1,29 @@
 import React, { Fragment } from "react";
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 import * as helpers from '/imports/ui/components/landing/components/jss/helpers.js';
-import FormGhostButton from '/imports/ui/components/landing/components/buttons/FormGhostButton.jsx';
 import { ContainerLoader } from "/imports/ui/loading/container";
 
-const Center = styled.div``;
+
+const emailSvgAnim = keyframes`
+   0% {
+     transform : rotate(0deg);
+   }
+   100% {
+     transform: rotate(360deg);
+   }
+}`;
 const Head = styled.div`
-  width: 200px;
-  margin: auto;
+  justify-content: center;
+  display: flex;
+  height: 64px;
+  background-color: white;
 `;
 const Logo = styled.div`
-  width: 50px;
+  width: 24px;
+  height: 24px;
   display: inline-block;
+  top: 20px;
+  position: relative;
 `;
 const Img = styled.img`
   width: 100%;
@@ -20,6 +32,8 @@ const Img = styled.img`
 const TextWrapper = styled.div`
   display: inline-block;
   padding: 20px 0;
+  display: flex;
+ 
 `;
 const H1 = styled.h1`
   font-weight: light;
@@ -34,8 +48,8 @@ const ButtonsWrapper = styled.div`
 `;
 const P = styled.p`
   font-size: 16px;
-  margin: 0;
-  line-height: 1;
+  margin: 1px 0px 0px 5px;
+  font-weight: 400;
 `;
 const EmailStatus = styled.div`
     width: fit-content;
@@ -43,11 +57,14 @@ const EmailStatus = styled.div`
     font-weight: lighter;
     font-size: 21px;
     background-color:${helpers.primaryColor};
-    padding: 5px 15px 5px 15px;
     border-radius: 25px;
     color: white;
     font-family: 'Zilla Slab',serif;
     box-shadow: 4px 11px 2px -4px #888888;
+    transition: all 0.3s ease-out;
+    visibility: ${props => props.emailSend ? "visible" : "hidden"};
+    padding: ${props => props.emailSend ? "5px 4% 5px 4%" : "0px"};
+    height: ${props => props.emailSend ? "35px" : "0px"};
 `;
 const GroupSvg = styled.div`
   background-image: url(${config.groupSvg});
@@ -56,8 +73,7 @@ const GroupSvg = styled.div`
   background-repeat: no-repeat;
   height: 138px;
   max-height: 600px;
-  width: auto;
-  margin-right:45%;
+  width: 121px;
   @media screen and (max-width: ${helpers.mobile}px) {
     margin-right:38%;
   }
@@ -66,56 +82,91 @@ const EmailSvg = styled.div`
   background-image: url(${config.emailSvg});
   background-position: 100% calc(100% - 14px);
   background-repeat: no-repeat;
-  height: 59px;
   max-height: 422px;
   width: 36px;
-  position: relative;
-  bottom: 100px;
- 
+  position: absolute;
+  transition: all 0.3s ease-out;
+  height: 59px;
+  top: 27%;
+  right: 34%;
+  ${props=> props.emailSend ? `animation: ${emailSvgAnim} 0.5s` : ''}
 `;
 const Text = styled.div`
   margin: auto;
   width: fit-content;
   font-family: "Zilla Slab", serif;
-  font-size: 18px;
-  font-weight: 400;
-  margin-top: -60px;
+  font-size: 48px;
+  color: #333;
+  line-height: 1.5;
+  margin-bottom: 24px;
+  @media screen and (max-width: ${helpers.mobile}px) {
+    font-size: 36px;
+  }
 `;
 const Summary = styled.div`
   margin: 10px auto auto auto;
-  width: 222px;
+  width: 355px;
   font-family: "Zilla Slab", serif;
-  font-size: 16px;
+  font-size: 24px;
   font-weight: lighter;
+  color: #555;
+  margin-bottom: 24px;
+`;
+const Counter = styled.div`
+  font-family: monospace;
+  margin:5px auto 5px auto;
+`;
+const Container = styled.div`
+background-color: #f2f2f2;
+`;
+const ImagesContainer = styled.div`
+position: relative;
+width: fit-content;
+margin-bottom: 24px;
+`;
+const CustomButton = styled.button`
+  background-color: #333;
+  color: #f2f2f2;
+  height: 48px;
+  min-width: 200px;
+  font-family: "Zilla Slab", serif;
+  font-size: 24px;
+  padding: 8px 16px;
+  box-sizing: border-box;
+  border: 2px;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 export function EmailVerifyDashboardRender () {
-  const {state:{email,isLoading},reSendEmailVerificationLink} = this;
+  const {state:{email,isLoading,disabled,emailSend},reSendEmailVerificationLink} = this;
   return (
     <Fragment>
+      <Container>
       {isLoading && <ContainerLoader/>}
         <Head>
           <Logo>
             <Img src={config.skillShapeLogo} />
           </Logo>
           <TextWrapper>
-            <H1>Skillshape,</H1>
-            <P>your path revealed</P>
+            <H1>Skillshape,</H1><P>your path revealed</P>
           </TextWrapper>
         </Head>
-        <EmailStatus>
+        <EmailStatus emailSend={emailSend}>
           An email send again!
         </EmailStatus>
-        <center> <GroupSvg /><EmailSvg /></center>
+        <center> <ImagesContainer><GroupSvg /><EmailSvg emailSend={emailSend}/></ImagesContainer></center>
         <Text> Please check your inbox!.</Text>
         <Summary> We just send a confirmation link to {email}.Click the link to sign in to your dashboard.</Summary>
         <center>
           <ButtonsWrapper>
-          <FormGhostButton
-            label={'Resend Email'}
+          <CustomButton
+            disabled={disabled}
             onClick={reSendEmailVerificationLink}
-          />
+          >Resend Email</CustomButton>
+          <Counter id="counter"/>
           </ButtonsWrapper>
         </center>
+        </Container>
     </Fragment>
   );
 };
--- imports/ui/pages/emailVerifyDashboard/index.js
+++ imports/ui/pages/emailVerifyDashboard/index.js
@@ -5,7 +5,7 @@ import {redirectToThisUrl,withPopUp} from '/imports/util';
 class EmailVerifyDashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = { emailSend:false};
   }
   
   componentWillMount() {
@@ -23,23 +23,41 @@ class EmailVerifyDashboard extends Component {
     }
   }
   reSendEmailVerificationLink = () => {
-    this.setState({ isLoading: true });
+    this.setState({ isLoading: true ,disabled:true,});
     const{popUp}= this.props;
     Meteor.call(
       "user.sendVerificationEmailLink",
       this.state.email,
       (err, res) => {
-        this.setState({ isLoading: false });
+        this.setState({ isLoading: false, emailSend:true});
         if (err) {
           let errText = err.reason || err.message;
           popUp.appear("error",{content: errText});
         }
         if (res) {
+          this.countdown();
           popUp.appear("success", {content: "We send a email verification link, Please check your inbox!!"});
         }
       }
     );
   };
+
+ countdown =() => {
+    let seconds = 10;
+     tick =() => {
+        let counter = document.getElementById("counter");
+        seconds--;
+        counter.innerHTML = "00:" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            counter.innerHTML = ``;
+            this.setState({disabled:false,emailSend:false});
+        }
+    }
+    tick();
+}
+
   render() {
   
     return (
