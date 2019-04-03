90d194aa5e052168053b61df4f12cbde9af7db25
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-04-03 14:33:04 +0530

    1.User Authication Rest Api added with all cases. `Done`

--- imports/api/user/server/restApis.js
+++ imports/api/user/server/restApis.js
@@ -1,23 +1,32 @@
-import  bodyParser from "body-parser";
+import bodyParser from "body-parser";
+import { isEmpty } from "lodash";
 Picker.middleware(bodyParser.json());
 Picker.middleware(bodyParser.urlencoded({ extended: false }));
 
-
-Picker.route("/v1/users/:email/:password",(params, request, response, next  )=>{
-    const {email,password} = params;
-    user = Meteor.users.findOne({ 'emails.address': email });
-    if(user){
-      let result = Accounts._checkPassword(user, password);
-			const {error} = result
-      if(!error){
-        response.end(JSON.stringify(result));
+Picker.route("/api/v1/users/", (params, req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    let payload = {};
+    if (email && password) {
+      user = Meteor.users.findOne({ "emails.address": email });
+      if (!isEmpty(user)) {
+        let result = Accounts._checkPassword(user, password);
+        const { error } = result;
+        if (!error) {
+          delete user.services;
+          payload = user;
+        } else if (error) {
+          const { reason } = error;
+          payload = { error:reason };
+        }
+      } else {
+        payload = { error: "User Not Exists" };
       }
-      else if(error){
-        response.end(JSON.stringify(error))
-      }
+    } else {
+      payload = { error: "Email and password is required." };
     }
-    else{
-      response.end('User Not Found')
-    }
-    })
-  
+    res.end(JSON.stringify(payload));
+  } catch (error) {
+    res.end(JSON.stringify({ error: error }));
+  }
+});
